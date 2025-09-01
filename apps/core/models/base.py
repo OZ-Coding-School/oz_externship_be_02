50a1875dd4d6b145041f9409a24f230d575c13ea
inu9431 <inu9431@naver.com>
2025-09-01 16:02:21 +0900

    🔥 Core models
    core models update

--- apps/core/models/base.py
+++ apps/core/models/base.py
@@ -4,11 +4,15 @@ from django.db import models
 
 
 class BaseModel(models.Model):
-    id = models.BigAutoField(primary_key=True, editable=False)
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True,
-                            verbose_name="UUID")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성일시")
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True, verbose_name="수정 일시")
 
     class Meta:
         abstract = True
+
+class UUIDBaseModel(BaseModel):
+    uuid =  uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, verbose_name="UUID")
+
+    class Meta:
+        abstract = True
+
