fa76c28a402a616880854a647e3b3c582d251e4a
inu9431 <inu9431@naver.com>
2025-09-02 13:58:07 +0900

    :recycle: user_model verbose_name -> help_text 변경

--- apps/users/models/social_user.py
+++ apps/users/models/social_user.py
@@ -8,9 +8,9 @@ class SocialUser(BaseModel):
     class ProviderChoices(models.TextChoices):
         KAKAO = "KAKAO", "카카오"
         NAVER = "NAVER", "네이버"
-    user = models.ForeignKey("users.user", on_delete=models.CASCADE, verbose_name="유저")
-    provider = models.CharField(max_length=10, choices=ProviderChoices.choices, verbose_name="소셜 로그인 제공 업체")
-    provider_id = models.CharField(max_length=255, null=False, verbose_name="소셜 로그인 업체 고유 ID")
+    user = models.ForeignKey("users.user", on_delete=models.CASCADE, help_text="유저")
+    provider = models.CharField(max_length=10, choices=ProviderChoices.choices, help_text="소셜 로그인 제공 업체")
+    provider_id = models.CharField(max_length=255, null=False, help_text="소셜 로그인 업체 고유 ID")
 
 
     def __str__(self) -> str:
