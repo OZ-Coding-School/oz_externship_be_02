5b3d1e01cf2658b6370b17a3de14dd8b426a22f7
inu9431 <inu9431@naver.com>
2025-09-02 14:18:37 +0900

    :recycle: basemodel verbose_name -> help_text 변경

--- apps/core/models/base.py
+++ apps/core/models/base.py
@@ -4,15 +4,15 @@ from django.db import models
 
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성일시")
-    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True, verbose_name="수정 일시")
+    created_at = models.DateTimeField(auto_now_add=True, help_text="생성일시")
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True, help_text="수정 일시")
 
     class Meta:
         abstract = True
 
 
 class UUIDBaseModel(BaseModel):
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, verbose_name="UUID")
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, help_text="UUID")
 
     class Meta:
         abstract = True
