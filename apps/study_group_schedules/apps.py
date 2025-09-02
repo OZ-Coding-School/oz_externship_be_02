7024f5569d48fe982e3e0139876f9e138aad6669
jclee5419 <jclee5419@gmail.com>
2025-09-02 14:01:18 +0900

    ✨ feat: 프로젝트 구조 생성(study_group_schedules, study_notes, studies)

---
+++ apps/study_group_schedules/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StudyGroupSchedulesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.study_group_schedules"
---
+++ apps/study_group_schedules/models/__init__.py
@@ -0,0 +1,7 @@
+from .schedule_participants import ScheduleParticipants
+from .study_group_schedules import GroupSchedule
+
+__all__ = [
+    "GroupSchedule",
+    "ScheduleParticipants",
+]
