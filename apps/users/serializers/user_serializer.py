49cd8f6c806d45920be9d038eff986956cfcd789
inu9431 <inu9431@naver.com>
2025-09-03 21:55:56 +0900

    🎨 style: black, isort 코드 포매터 실행

--- apps/users/serializers/user_serializer.py
+++ apps/users/serializers/user_serializer.py
@@ -3,6 +3,6 @@ from typing import Any
 from rest_framework import serializers
 
 
-class VerificationSerializer(serializers.Serializer): # type: ignore
+class VerificationSerializer(serializers.Serializer):  # type: ignore
     email = serializers.EmailField(required=True)
     verification_code = serializers.CharField(required=True)
