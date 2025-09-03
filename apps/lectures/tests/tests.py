1f3fc24c2827c85a06edda4ee1531476a93fbb78
park3hho <ejfhr7777@gmail.com>
2025-09-03 16:10:04 +0900

    # 💡 build: <REQ-LECT-001, 강의 목록 조회 API>
    # 2. 변경 또는 추가사항을 아래에 간략하게 작성하세요 ( 필수 )
    [1] 💡 REQ-LECT-001, 강의 목록 조회 API 작성
    [2] 💡
    [3] 💡
    [4] 💡
    
    # 3. 이슈 ( 선택 )
    # 관련 이슈: #123
    
    # 0.1.1 #
    ========================================================================
    # Commit Convention
    # ✨ feat     : 새로운 기능 추가
    # 🐛 fix      : 버그 수정
    # 💡 chore    : 기능 추가 없이 코드 수정 (오타, 주석 등)
    # 📝 docs     : 문서 수정 (README 등)
    # 🚚 build    : 빌드 관련 파일 수정
    # ✅ test     : 테스트 코드 추가/변경 (프로덕션 코드 변경 없음)
    # ♻️ refactor : 리팩터링 (기능 변화 없음)
    # 🚑 hotfix   : 긴급 수정
    
    # Conflicts:
    #	apps/lectures/migrations/0004_alter_category_created_at_alter_category_updated_at_and_more.py
    
    # Conflicts:
    #	config/urls.py
    
    # Conflicts:
    #	config/urls.py
    
    # Conflicts:
    #	config/urls.py

--- apps/lectures/tests/tests.py
+++ apps/lectures/tests/tests.py
@@ -1,34 +1,60 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from unittest.mock import patch
-from lectures.models import Lecture
+from apps.lectures.models.crawled_lectures import Lecture
+import uuid
+from django.urls import reverse
 
 class LectureTestCase(TestCase):
     def setUp(self):
         # Given
-        self.lecture_id = 1
-        self.lecture = Lecture.objects.create(
-            id=self.lecture_id,
+        self.client = Client()
+        self.list_url = reverse('lectures:lecture_list')
+        self.lecture1 = Lecture.objects.create(
+            id=1,
+            uuid=uuid.uuid4(),
             title="테스트 강의",
             instructor="코딩 파트너",
             average_rating=5.0,
-            thumbnail_img_url="http://example.com/img.jpg",
+            duration=100,
             difficulty="초급",
+            description="PARKCODING",
+            platform="유데미",
             original_price=10000,
             discount_price=5000,
-            platform="유데미",
-            url_link="http://example.com/lecture/1"
+            url_link="http://example.com/lecture/1",
+            thumbnail_img_url="http://example.com/img.jpg",
         )
-    def test_lecture_creation(self):
-        retrieved_lecture = Lecture.objects.get(id=self.lecture_id)
-        self.assertEqual(retrieved_lecture.title, "테스트 강의")
-        self.assertEqual(retrieved_lecture.instructor, "코딩 파트너")
-        self.assertEqual(retrieved_lecture.original_price, 10000)
+        self.lecture2 = Lecture.objects.create(
+            id=2,
+            uuid=uuid.uuid4(),
+            title="테스트 강의2",
+            instructor="코딩 파트너2",
+            average_rating=4.0,
+            duration=80,
+            difficulty="초급",
+            description="NOTPARKCODING",
+            platform="인프런",
+            original_price=20000,
+            discount_price=1000,
+            url_link="http://example.com/lecture/2",
+            thumbnail_img_url="http://example.com/img2.jpg",
+        )
 
-    def test_lecture_title_is_correct(self):
-        retrieved_lecture = Lecture.objects.get(title="테스트 강의")
-        self.assertEqual(retrieved_lecture.title, self.lecture.title)
-    # When
+    def test_lecture_list_api(self):
+        # When
+        response = self.client.get(self.list_url)
 
+        # Then
+        self.assertEqual(response.status_code, 200)
+        response_data = response.json()
+        self.assertEqual(len(response_data), 2)
+
+        first_lecture_data = response_data[0]
+        self.assertEqual(first_lecture_data['title'], self.lecture1.title)
+        self.assertEqual(first_lecture_data['instructor'], self.lecture1.instructor)
+        second_lecture_data = response_data[1]
+        self.assertEqual(second_lecture_data['title'], self.lecture2.title)
+        self.assertEqual(second_lecture_data['instructor'], self.lecture2.instructor)
 
     #
     # def test_get_lecture_list(self):
