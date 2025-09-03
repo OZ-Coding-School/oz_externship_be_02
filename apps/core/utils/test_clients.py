1c9e8f44f1c7869f8c758fdc8dae6b620e6e50c8
meoyoug <eodyd426@gmail.com>
2025-09-03 14:14:43 +0900

    💡 chore: mypy 타입어노테이션 추가

--- apps/core/utils/test_clients.py
+++ apps/core/utils/test_clients.py
@@ -1,6 +1,6 @@
-from rest_framework.test import APITestCase
 from django.test import override_settings
-from django_redis import get_redis_connection
+from django_redis import get_redis_connection  # type: ignore
+from rest_framework.test import APITestCase
 
 
 @override_settings(
@@ -8,18 +8,13 @@ from django_redis import get_redis_connection
         "default": {
             "BACKEND": "django_redis.cache.RedisCache",
             "LOCATION": "redis://localhost:6379/15",  # test redis
-            "OPTIONS": {
-                "CLIENT_CLASS": "django_redis.client.DefaultClient"
-            },
+            "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
         }
     }
 )
 class RedisTestClient(APITestCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.redis_client = get_redis_connection("default")
+    redis_client = get_redis_connection("default")
 
-    def tearDown(self):
+    def tearDown(self) -> None:
         self.redis_client.flushdb()  # 현재 DB만 초기화
         super().tearDown()
