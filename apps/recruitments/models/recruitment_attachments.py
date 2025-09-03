bf9bcef09c8df4b55d5bdae41af86b0658a73267
Jiwoong0322 <woong4624@gmail.com>
2025-09-03 15:46:15 +0900

    💡 chore: recruitments_attachments.py 수정
    1) commit후 checkout을 하려고 하니 recruitment_attachments에서
       error발생. 단순 줄바꿈 때문으로 보임.
    
    관련 이슈: #25

--- apps/recruitments/models/recruitment_attachments.py
+++ apps/recruitments/models/recruitment_attachments.py
@@ -14,7 +14,7 @@ class RecruitmentAttachment(BaseModel):
         help_text="스터디 공고 ID",
     )
     # URLField -> FileField 로 변경, upload_to 경로 지정
-    # FileField를 사용하여 사용자가 업로드한 '파일 자체'를 서버의 지정된 위치에 물리적으로 젖아하는 역할. 파일이 저장될 '지정된 위치'는 setting에서 지정한다.
+    # FileField를 사용하여 사용자가 업로드한 '파일 자체'를 서버의 지정된 위치에 물리적으로 저장하는 역할. 파일이 저장될 '지정된 위치'는 setting에서 지정한다.
     file_url = models.FileField(upload_to="recruitment_attachments/%Y/%m/%d/", help_text="첨부파일")
     file_name = models.CharField(max_length=100, help_text="원본 파일명")  # 파일명은 별도 저장
 
