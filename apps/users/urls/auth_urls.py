1e15f01ffbcb64be18085108e678abfcb840b889
inu9431 <inu9431@naver.com>
2025-09-03 15:06:30 +0900

    :sparkles: users/urls 작성 smtp설정 추가 test_code 작성
    # Conflicts:
    #	config/urls.py

---
+++ apps/users/urls/auth_urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from apps.users.views import auth_view
+from apps.users.views.auth_view import VerificationView
+
+urlpatterns = [
+    path('verify-email/', VerificationView.as_view(), name='verify_email'),
+]
