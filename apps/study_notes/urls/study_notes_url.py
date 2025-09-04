7c620658df6160dc3f5ff5348275bd4b993ab435
박준혁 <jun201908@naver.com>
2025-09-04 14:37:41 +0900

    ♻️ refactor : 리팩터링 (urls에 app_name 추가)
    2. 변경 또는 추가사항을 아래에 간략하게 작성하세요 ( 필수 )
    - apps.urls에 app_name 추가
    - Config에 namespace 추가

--- apps/study_notes/urls/study_notes_url.py
+++ apps/study_notes/urls/study_notes_url.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 from apps.study_notes.views.study_notes_views import StudyNoteCreateView
 
+app_name = "study_notes"
+
 urlpatterns = [
     # Post /api/v1/study/<group_uuid>/notes/ - 스터디 기록 생성
     path("/<uuid:group_uuid>/notes", StudyNoteCreateView.as_view(), name="create-study-note"),
--- config/urls.py
+++ config/urls.py
@@ -10,7 +10,6 @@ from drf_spectacular.views import (
 urlpatterns: list[URLPattern | URLResolver] = [
     path("api/v1/notifications", include("apps.notifications.urls")),
     path("api/v1/studies/", include("apps.study_notes.urls")),
-
 ]
 
 if settings.DEBUG:
