32ca056538764ad3df72a4fa593b06160ddedfec
inu9431 <inu9431@naver.com>
2025-09-04 17:02:17 +0900

    💡 chore: 시리얼라이저 분리 및 이메일과 코드받는 시리얼라이저추가

---
+++ apps/users/serializers/email_verification_serializers.py
@@ -0,0 +1,12 @@
+from typing import Any
+
+from rest_framework import serializers
+
+
+class EmailVerificationRequestSerializer(serializers.Serializer[dict[str, Any]]):
+    email = serializers.EmailField(required=True)
+
+
+class EmailVerifyCodeSerializer(serializers.Serializer[dict[str, Any]]):
+    email = serializers.EmailField(required=True)
+    code = serializers.CharField(required=True)
--- apps/users/serializers/user_serializer.py
+++
@@ -1,8 +0,0 @@
-from typing import Any
-
-from rest_framework import serializers
-
-
-class VerificationSerializer(serializers.Serializer):  # type: ignore
-    email = serializers.EmailField(required=True)
-    verification_code = serializers.CharField(required=True)
