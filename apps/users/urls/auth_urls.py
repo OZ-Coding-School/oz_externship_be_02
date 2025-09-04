82daa81dc974766400ca2bda587dbf337842e41c
inu9431 <inu9431@naver.com>
2025-09-04 11:07:56 +0900

    🐛 fix: urls trailing slash 제거

--- apps/users/urls/auth_urls.py
+++ apps/users/urls/auth_urls.py
@@ -4,5 +4,5 @@ from apps.users.views import auth_view
 from apps.users.views.auth_view import VerificationView
 
 urlpatterns = [
-    path("verify-email/", VerificationView.as_view(), name="verify_email"),
+    path("verify-email", VerificationView.as_view(), name="verify_email"),
 ]
