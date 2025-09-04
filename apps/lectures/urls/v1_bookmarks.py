abf65ddcbbe7bfa5cfb180fda4c191e401c9070f
Kochiya35 <srayhjk@naver.com>
2025-09-04 15:06:18 +0900

    💡 chore: config/urls, lecture/urls trailing slash 제거
    config와 lecture의 url에서 마지막에 있는 슬래쉬를 제거했습니다.
    
    # Conflicts:
    #	config/urls.py

--- apps/lectures/urls/v1_bookmarks.py
+++ apps/lectures/urls/v1_bookmarks.py
@@ -3,5 +3,5 @@ from django.urls import path
 from apps.lectures.views.bookmark_views import BookmarkToggleView
 
 urlpatterns = [
-    path("bookmarks/toggle", BookmarkToggleView.as_view()),
+    path("/bookmarks/toggle", BookmarkToggleView.as_view()),
 ]
