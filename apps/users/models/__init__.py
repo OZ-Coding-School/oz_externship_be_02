dfcf3649310a04af6b4f95e740b16dcaa08123d7
inu9431 <inu9431@naver.com>
2025-09-04 11:06:20 +0900

    💡 chore: 파일 이름이랑 클래스명 달라서 수정

--- apps/users/models/__init__.py
+++ apps/users/models/__init__.py
@@ -1,5 +1,5 @@
 from .social_user import SocialUser
 from .user import User
-from .withdrawals import WithdrawalsReasonChoices, Withdrwals
+from .withdrawals import WithdrawalsReasonChoices, Withdrawals
 
 __all__ = ["User"]
--- apps/users/models/withdrawals.py
+++ apps/users/models/withdrawals.py
@@ -17,7 +17,7 @@ class WithdrawalsReasonChoices(models.TextChoices):
     OTHER = "OTHER", "기타"
 
 
-class Withdrwals(BaseModel):
+class Withdrawals(BaseModel):
     user = models.OneToOneField("users.User", on_delete=models.CASCADE, unique=True, help_text="유저")
     reason = models.CharField(
         max_length=30, choices=WithdrawalsReasonChoices.choices, null=False, blank=False, help_text="탈퇴 사유"
