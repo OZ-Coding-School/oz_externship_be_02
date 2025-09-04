7c0c3caf125ea2fa020f135f79bfe72bde6f0377
inu9431 <inu9431@naver.com>
2025-09-04 11:10:44 +0900

    🐛 fix: 디벨롭충돌 해결 및 urls 추가

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ from drf_spectacular.views import (
 )
 
 urlpatterns: list[URLPattern | URLResolver] = [
+    path("api/v1/", include("apps.users.urls.auth_urls")),
     path("api/v1/notifications", include("apps.notifications.urls")),
 ]
 
