96da69ded54be5403d09e4015f027242d9f96f4b
yoo-sh-96 <tmdguq90051@gmail.com>
2025-09-04 18:12:39 +0900

    🎨 style: black, isort 코드 포매터 재실행
    ci 실패로 인한 black 재실행
    
    관련 이슈: #26

--- apps/notifications/tests.py
+++ apps/notifications/tests.py
@@ -67,6 +67,5 @@ class NotificationViewsTests(APITestCase):
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn("unread_count", response.data) # unread_count 키값이 있는지 확인
-        self.assertIsInstance(response.data["unread_count"], int) # unread_count가 int면 통과
-
+        self.assertIn("unread_count", response.data)  # unread_count 키값이 있는지 확인
+        self.assertIsInstance(response.data["unread_count"], int)  # unread_count가 int면 통과
