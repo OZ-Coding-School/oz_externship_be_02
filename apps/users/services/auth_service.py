438aece23028e1ba66effd47c0653ebcb623e8e1
inu9431 <inu9431@naver.com>
2025-09-04 17:00:22 +0900

    💡 chore: 서비스 코드에서 이메일 인증코드생성 방식 수정 및 클래스 이름 수정

--- apps/users/services/auth_service.py
+++ apps/users/services/auth_service.py
@@ -1,49 +1,39 @@
-import base64
-import os
-import secrets
-from typing import TYPE_CHECKING, Any, Dict
+import uuid
+from smtplib import SMTPException
 
 from django.conf import settings
 from django.core.cache import cache
 from django.core.mail import send_mail
-from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
+from rest_framework import status
+from rest_framework.response import Response
 
-from apps.users.models import User
-
-if TYPE_CHECKING:
-    from typing import Final
+from apps.core.utils.base62 import Base62
 
 
-def verify_user_email(email: str, verification_code: str) -> User:
-    try:
-        user = User.objects.get(email=email)
-    except User.DoesNotExist:
-        raise ValidationError({"email": ["존재하지않는 이메일입니다"]})
+class EmailVerificationService:
+    def verify_code(self, email: str, code: str) -> Response:
+        cached_verification_code = cache.get(email)
 
-    cached_verification_code = cache.get(email)
-    if cached_verification_code is None:
-        raise ValidationError({"verification_code": ["인증 코드가 만료되었거나 존재하지 않습니다"]})
+        if cached_verification_code != code:
+            return Response({"error": "인증번호가 일치하지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
-    if cached_verification_code != verification_code:
-        raise ValidationError({"verification_code": ["인증코드가 일치하지않습니다"]})
+        cache.delete(email)
+        return Response({"detail": "이메일 인증 성공"}, status=status.HTTP_200_OK)
 
-    user.is_active = True
-    user.save()
-    cache.delete(email)
-    return user
+    def send_verification_email(self, email: str) -> Response:
+        verification_code = self.generate_verification_code()
+        subject = "회원가입 이메일 인증"
+        message = f"인증 코드를 입력하여 회원가입을 완료해주세요: {verification_code}"
+        from_email = settings.EMAIL_HOST_USER
+        recipient_list = [email]
+        cache.set(email, verification_code, timeout=3600)
 
+        try:
+            send_mail(subject, message, from_email, recipient_list)
+            return Response({"detail": "입력하신 이메일로 인증번호가 발송되었습니다."}, status=status.HTTP_200_OK)
+        except SMTPException:
+            cache.delete(email)
+            return Response({"error": "이메일 발송 중 예외가 발생하였습니다."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-def send_verification_email(email: str, verification_code: str) -> None:
-    subject = "회원가입 이메일 인증"
-    message = f"인증 코드를 입력하여 회원가입을 완료해주세요: {verification_code}"
-    from_email = settings.EMAIL_HOST_USER
-    recipient_list = [email]
-
-    send_mail(subject, message, from_email, recipient_list)
-
-
-def generate_verification_code(email: str) -> str:
-    verification_code = base64.urlsafe_b64encode(secrets.token_bytes(9)).decode("utf-8")
-    cache.set(email, verification_code, timeout=3600)
-    return verification_code
+    def generate_verification_code(self) -> str:
+        return Base62.uuid_encode(u=uuid.uuid4())
