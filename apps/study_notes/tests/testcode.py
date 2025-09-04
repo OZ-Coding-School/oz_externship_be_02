79ca139c803b27c061acbe62711b64b8a1733615
박준혁 <jun201908@naver.com>
2025-09-04 15:54:52 +0900

    ✅ test     : 테스트 코드 추가(스터디 그룹 기록 작성)
    2. 변경 또는 추가사항을 아래에 간략하게 작성하세요 ( 필수 )
    - 기록 작성 시 필수값 누락 시 나오는 에러 확인 코드

--- apps/study_notes/tests/testcode.py
+++ apps/study_notes/tests/testcode.py
@@ -21,10 +21,35 @@ class TestCode(TestCase):
         self.users = data["users"]
         self.groups = data["groups"]
         self.notes = data["notes"]
-        self.user = self.users[0]
-        self.group = self.groups[0]
+        self.user = self.users[1]
+        self.group = self.groups[1]
         self.client.force_authenticate(user=self.user)
         self.url = reverse("create-study-note", kwargs={"group_uuid": self.group.uuid})
+
+    def test_create_study_note_false(self) -> None:
+        """
+        필수값에 대한 실패 테스트
+        """
+        payload = {
+            "title": "",
+            "content": "오늘은 TDD 작성법에 대해 공부했습니다.",
+            "images": ["https://example.com/image1.png"],
+            "attachments": [{"file_url": "https://example.com/file1.pdf", "file_name": "file1.pdf"}],
+        }
+        response = cast(Response, self.client.post(self.url, payload, format="json"))
+        print("status:", response.status_code)
+        print("request.data 전달된 값:", payload)
+        print("response.data:", response.data)
+        # 상태 코드 검증
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # 응답 데이터 검증
+        self.assertEqual(response.data["title"], payload["title"])
+        self.assertEqual(response.data["content"], payload["content"])
+        self.assertEqual(response.data["ai_summary"], "AI 요약은 추후 자동 생성됩니다.")
+
+        # DB 데이터 검증
+        self.assertTrue(StudyNote.objects.filter(title=payload["title"]).exists())
 
     def test_create_study_note_success(self) -> None:
         """
