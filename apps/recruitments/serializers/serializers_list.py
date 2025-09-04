1dfe278a2d6c79bca1892640f71e35f12c4dd76f
daeun0608 <glace0608@naver.com>
2025-09-04 17:01:30 +0900

    ♻️refactor : 시리얼라이저 이름 변경 및 전체 read_only설정 - class이름에 List를 추가해 디테일하게 표기 - Swagger-ui에 사용못하는 SerializerMethodField변경 - uuid 추가 - 조회기능이기 때문에 read_only설정을 전체적으로 적용 관련 이슈: #32

--- apps/recruitments/serializers/serializers_list.py
+++ apps/recruitments/serializers/serializers_list.py
@@ -6,33 +6,34 @@ from apps.recruitments.models.recruitments import Recruitment
 from apps.recruitments.models.tags import Tag
 
 
-# 비직관적인 이름 변경과 다대다 관계 처리
-class LectureSerializer(serializers.ModelSerializer[Lecture]):
+# 비직관적인 이름(스터디그룹->강의) 변경
+class LectureListSerializer(serializers.ModelSerializer[Lecture]):
     class Meta:
         model = Lecture
         fields = ("title", "instructor")
 
 
-class RecruitmentSerializer(serializers.ModelSerializer[Recruitment]):
+class RecruitmentListSerializer(serializers.ModelSerializer[Recruitment]):
     # 썸네일 이미지: 조회할 때 추가 ( 첫번째 이미지, 없는 경우 기본 )
-    img = serializers.URLField(read_only=True)
+    img = serializers.URLField()
 
-    # 하위에서 비직관적인 이름 변경과 다대다 관계 처리, 강의 목록 ( 강의명, 강사명 )
+    # 하위에서 비직관적인 이름 변경, 강의 목록 ( 강의명, 강사명 )
     # recruiments-study_groups-study_lectures-crawled_lectures > study_group__lectures로 최적화
     # queryset.select_related('study_group').prefetch_related('study_group__lectures')
     # 쿼리 최적화에 사용( https://docs.djangoproject.com/en/5.2/ref/models/querysets/#only )
-    lectures = serializers.SerializerMethodField(read_only=True)
+    lectures = LectureListSerializer(source="study_group.lectures", many=True)
 
     # tags는 Tag의 str이용
-    tags: StringRelatedField[Tag] = serializers.StringRelatedField(many=True, read_only=True)
+    tags: StringRelatedField[Tag] = serializers.StringRelatedField(many=True)
 
     # bookmarks_count는 bookmark_users의 개수만 받기 ( 조회 때 추가 )
-    bookmarks_count = serializers.IntegerField(read_only=True)
+    bookmarks_count = serializers.IntegerField()
 
     class Meta:
         model = Recruitment
         fields = (
             "id",
+            "uuid",
             "title",
             "img",
             "expected_headcount",
@@ -42,8 +43,4 @@ class RecruitmentSerializer(serializers.ModelSerializer[Recruitment]):
             "views_count",
             "bookmarks_count",
         )
-
-    def get_lectures(self, obj: Recruitment) -> list[object]:
-        lectures = obj.study_group.lectures.all()
-        serializer = LectureSerializer(lectures, many=True)
-        return list(serializer.data)
+        read_only_fields = fields
