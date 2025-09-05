0d66285adaafd4adfd899081c636f801a6d7a46d
meoyoug <eodyd426@gmail.com>
2025-09-05 19:03:10 +0900

    🚚 build: 개발 서버 및 운용 환경 로깅 설정 추가 및 수정

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -44,12 +44,7 @@ LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "filters": {
-        "require_debug_false": {
-            "()": "django.utils.log.RequireDebugFalse",
-        },
-        "require_debug_true": {
-            "()": "django.utils.log.RequireDebugTrue",
-        },
+        "require_debug_true": {"()": "django.utils.log.RequireDebugTrue"},
     },
     "formatters": {
         "simple": {
@@ -63,18 +58,19 @@ LOGGING = {
         },
     },
     "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "filters": ["require_debug_true"],
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
+        },
         "file": {
             "level": "INFO",
             "class": "logging.handlers.RotatingFileHandler",
             "filename": os.path.join(LOG_ROOT, "django.log"),
             "formatter": "simple",
             "maxBytes": 1024 * 1024 * 5,
-            "backupCount": 5,
-        },
-        "console": {
-            "level": "INFO",
-            "filters": ["require_debug_true"],
-            "class": "logging.StreamHandler",
+            "backupCount": 10,
         },
         "django.server": {
             "level": "INFO",
