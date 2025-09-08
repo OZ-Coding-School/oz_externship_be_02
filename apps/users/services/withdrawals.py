4b1528a3cb28ffc4886247678f00aa9a2aa4dbf9
c0z-y-fiction <cosm0snnt@gmail.com>
2025-09-08 17:02:24 +0900

    ✨ feat: 회원 탈퇴 서비스(services/withdrawals.py) 구현
    이미 탈퇴 요청을 한 계정이 다시 탈퇴를 요청할 시 에러 반환, 탈퇴 신청 시 즉시 해당 계정을 비활성화

--- apps/users/services/withdrawals.py
+++ apps/users/services/withdrawals.py
@@ -1,16 +1,20 @@
 # apps/users/services/withdrawals.py
 
-from datetime import date, timedelta
-from apps.users.models.withdrawals import Withdrwals
+
+from rest_framework import status
+from rest_framework.response import Response
+
+from apps.users.models.user import User
+from apps.users.models.withdrawals import Withdrawals
 
 
-# 사용자 정보와 탈퇴 대기 기간을 validated_data에 포함
-def request_withdrawal(user, validated_data: dict) -> Withdrwals:
-    validated_data["user"] = user
-    validated_data["due_date"] = date.today() + timedelta(days=14)
+# ---------- 탈퇴 신청(일반/소셜 구분) ----------
+def withdrawal_user(user: User, reason: str, reason_detail: str) -> Response:
+    if Withdrawals.objects.filter(user=user).exists():
+        return Response({"error": "이미 탈퇴 요청이 존재합니다."}, status=status.HTTP_400_BAD_REQUEST)
 
-    withdrawal = Withdrwals.objects.create(**validated_data)
+    # 탈퇴 신청 즉시 비활성화 -> 저장
+    user.is_active = False
+    user.save()
 
-    # 탈퇴 요청 즉시 계정을 비활성화할 것은 아니기 때문에 사용자의 active 상태는 확인하지 않음
-    #! 문제: 탈퇴 대기 기간 동안 활동이 가능한 것인지?
-    return withdrawal
+    return Response({"회원 탈퇴가 완료되었습니다. 계정은 14일 후에 삭제됩니다."}, status=status.HTTP_200_OK)
