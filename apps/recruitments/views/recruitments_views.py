b63e6a49e66ce6552750708a0581e2258b27523a
최재현 <zip020314@gmail.com>
2025-09-08 10:55:31 +0900

    💡 chore: path변수로 받는 recruiments_id -> recruiments_uuid로 변경

--- apps/recruitments/views/recruitments_views.py
+++ apps/recruitments/views/recruitments_views.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
@@ -13,14 +15,14 @@ from apps.recruitments.serializers.recruitments_serializers import (
 class RecruitmentDetailView(APIView):
     permission_classes = [AllowAny]
 
-    def get(self, request: Request, recruitment_id: int) -> Response:
+    def get(self, request: Request, recruitment_uuid: UUID) -> Response:
         # 테이블 명세서대로 하기 -> model과 serializer 이용하면 해결될거에요
         try:
-            # 1. URL 경로로 recruitment_id를 사용해 데이터베이스에 해당하는 Recruitment 객체 조회
+            # 1. URL 경로로 recruitment_uuid를 사용해 데이터베이스에 해당하는 Recruitment 객체 조회
             recruitment = (
                 Recruitment.objects.select_related("author")
                 .prefetch_related("tags", "attachments", "bookmark_users")
-                .get(id=recruitment_id)
+                .get(uuid=recruitment_uuid)
             )
         except Recruitment.DoesNotExist:
             return Response({"error": "해당 공고를 찾을 수 없음."}, status=status.HTTP_404_NOT_FOUND)
