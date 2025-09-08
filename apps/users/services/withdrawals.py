2b8c6a84bef768e8ec9052d29863e96ee380fc2e
c0z-y-fiction <cosm0snnt@gmail.com>
2025-09-08 17:55:07 +0900

    💡 chore: 사용하지 않는 줄 알고 삭제했던 코드 다시 추가
    18~19번째 줄 사용하지 않는 줄 알고 삭제했다가 테스트 코드 에러 띄워서 다시 추가...

--- apps/users/services/withdrawals.py
+++ apps/users/services/withdrawals.py
@@ -1,17 +1,22 @@
 # apps/users/services/withdrawals.py
 
+from datetime import date, timedelta
 
 from rest_framework import status
 from rest_framework.response import Response
 
+from apps.users.models.social_user import SocialUser
 from apps.users.models.user import User
-from apps.users.models.withdrawals import Withdrawals
+from apps.users.models.withdrawals import Withdrawals, WithdrawalsReasonChoices
 
 
 # ---------- 탈퇴 신청(일반/소셜 구분) ----------
 def withdrawal_user(user: User, reason: str, reason_detail: str) -> Response:
     if Withdrawals.objects.filter(user=user).exists():
         return Response({"error": "이미 탈퇴 요청이 존재합니다."}, status=status.HTTP_400_BAD_REQUEST)
+
+    due_date = date.today() + timedelta(days=14)
+    withdrawal = Withdrawals.objects.create(user=user, reason=reason, reason_detail=reason_detail, due_date=due_date)
 
     # 탈퇴 신청 즉시 비활성화 -> 저장
     user.is_active = False
