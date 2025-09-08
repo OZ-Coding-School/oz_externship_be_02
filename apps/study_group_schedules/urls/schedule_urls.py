35d87a79800a1e6c2def6551dee6629a0d5bb979
jclee5419 <jclee5419@gmail.com>
2025-09-08 14:23:20 +0900

    ✨ feat     : study_group_schedule 스케줄 생성 부분 endpoint url 작성

---
+++ apps/study_group_schedules/urls/schedule_urls.py
@@ -0,0 +1,12 @@
+from typing import List
+
+from django.urls import URLPattern, path
+
+from apps.study_group_schedules import views
+
+app_name = "study_group_schedules"
+
+urlpatterns: List[URLPattern] = [
+    # POST api/v1/schedules/
+    path("", views.StudyGroupScheduleCreateView.as_view(), name="create_schedule"),
+]
