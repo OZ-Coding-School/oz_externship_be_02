09c6959d838765c157c66a06a6c29a9d44738c42
c0z-y-fiction <cosm0snnt@gmail.com>
2025-09-08 18:35:30 +0900

    💡 chore: urlpatterns path 직관적으로 수정
    path delete -> withdraw로 변경

--- apps/users/urls/auth_urls.py
+++ apps/users/urls/auth_urls.py
@@ -9,5 +9,5 @@ from apps.users.views.withdrawals import WithdrawalAPIView
 urlpatterns = [
     path("email/send-code", EmailVerificationSendAPIView.as_view(), name="send_code_email"),
     path("email/verify", EmailVerifyCodeAPIView.as_view(), name="email_verify_code"),
-    path("delete", WithdrawalAPIView.as_view(), name="account_withdrawals"),
+    path("withdraw", WithdrawalAPIView.as_view(), name="account_withdrawals"),
 ]
