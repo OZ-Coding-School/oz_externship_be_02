07404d435bcddfcd19d619c6d4c79b51d8a03077
park3hho <ejfhr7777@gmail.com>
2025-09-08 14:39:20 +0900

    # 💡✅test: <CICD>
    # 2. 변경 또는 추가사항을 아래에 간략하게 작성하세요 ( 필수 )
    [1] 💡✅ CI, TEST 통과하게끔 재작성함.
    [2] ✨
    [3] ✨
    [4] 💡
    
    # 3. 이슈 ( 선택 )
    # 관련 이슈: #123
    
    # 4. 메모 # 0.3.1 #
    # 다음부턴 테스트 코드 먼저 작성하자
    #
    # hahamemo.txt 올리면 안됨
    # cached_lecture_list.py 올리면 안됨 (Scripts)
    # paginate, sort_lectures, filter_lectures 어디에 분리? -> PR로 올려서 검토 받기
    # Redis에 넣는 로직 자동화:
    - (1) Log 관련 기능 완성하면 사용자 관련 추천 서비스와 연동하여, 검색할 때마다 추천 강의를 캐시에 넣도록 수정해야함.
    - (2) 데이터베이스에 새로운 강의 목록이 추가되면 캐시 데이터를 수정해야함.
    ========================================================================
    # Commit Convention
    # ✨ feat     : 새로운 기능 추가
    # 🐛 fix      : 버그 수정
    # 💡 chore    : 기능 추가 없이 코드 수정 (오타, 주석 등)
    # 📝 docs     : 문서 수정 (README 등)
    # 🚚 build    : 빌드 관련 파일 수정
    # ✅ test     : 테스트 코드 추가/변경 (프로덕션 코드 변경 없음)
    # ♻️ refactor : 리팩터링 (기능 변화 없음)
    # 🚑 hotfix   : 긴급 수정

--- apps/lectures/serializers/category.py
+++ apps/lectures/serializers/category.py
@@ -1,5 +1,7 @@
 from rest_framework.serializers import ModelSerializer
+
 from apps.lectures.models.categories import Category
+
 
 class CategorySerializer(ModelSerializer[Category]):
     class Meta:
