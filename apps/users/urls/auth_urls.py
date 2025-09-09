7d42776a0ac12ac614f2ade2c616bcf5bfb97cc1
inu9431 <inu9431@naver.com>
2025-09-09 17:04:12 +0900

    💡 chore: 클래스명 변경되서 변경된 값에 맞게 수정

--- apps/users/urls/auth_urls.py
+++ apps/users/urls/auth_urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 
 from apps.users.views.email_view import (
-    EmailVerificationSendAPIView,
-    EmailVerifyCodeAPIView,
+    SignUpEmailVerificationSendAPIView,
+    SignUpEmailVerifiCationVerifyAPIView,
 )
 from apps.users.views.phone_view import SendVerificationCodeAPIView, VerifyCodeAPIView
 from apps.users.views.withdrawals import WithdrawalAPIView
 
 urlpatterns = [
-    path("email/send-code", EmailVerificationSendAPIView.as_view(), name="email_send_code"),
-    path("email/verify", EmailVerifyCodeAPIView.as_view(), name="email_verify_code"),
+    path("email/send-code", SignUpEmailVerificationSendAPIView.as_view(), name="email_send_code"),
+    path("email/verify", SignUpEmailVerifiCationVerifyAPIView.as_view(), name="email_verify_code"),
     path("phone/send-code", SendVerificationCodeAPIView.as_view(), name="phone_send_code"),
     path("phone/verify-code", VerifyCodeAPIView.as_view(), name="phone_verify_code"),
     path("withdraw", WithdrawalAPIView.as_view(), name="account_withdrawals"),
