b46f2244c9e663f1e8f7efc21bf7c9c8180cf25b
inu9431 <inu9431@naver.com>
2025-09-09 14:05:35 +0900

    💡 chore    : 타입에러떄문에 type_checking 임포트 추가

--- apps/users/managers/withdrawals_user_manager.py
+++ apps/users/managers/withdrawals_user_manager.py
@@ -1,8 +1,13 @@
+import datetime
+from typing import TYPE_CHECKING
 from django.db import models
 from django.utils import timezone
+from datetime import timedelta
 
-class WithdrawalUserManager(models.Manager):
+if TYPE_CHECKING:
+    from apps.users.models.withdrawals import Withdrawals
+class WithdrawalUserManager(models.Manager["Withdrawals"]):
     # 2주 내에 탈퇴한 유저
-    def recently_withdrawn(self):
-        two_weeks_ago = timezone.now() - timezone.timedelta(day=14)
+    def recently_withdrawn(self) -> models.QuerySet["Withdrawals"]:
+        two_weeks_ago = timezone.now() - datetime.timedelta(weeks=2)
         return self.filter(due_date__gte=two_weeks_ago)
