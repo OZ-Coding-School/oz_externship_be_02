08c236a31fd59308019bb5e6d63a40c699316788
inu9431 <inu9431@naver.com>
2025-09-09 14:59:26 +0900

    💡 chore: 유저매니저를 managers에 통합해서 정의

--- apps/users/models/user.py
+++ apps/users/models/user.py
@@ -7,29 +7,7 @@ from django.contrib.auth.models import (
 from django.db import models
 
 from apps.core.models.base import UUIDBaseModel
-
-
-class UserManager(BaseUserManager["User"]):
-    def create_user(self, email: str, password: str, **extra_fields: Any) -> "User":
-        if not email:
-            raise ValueError("Users must have an email address")
-
-        user = self.model(email=self.normalize_email(email), **extra_fields)
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email: str, password: str, **extra_fields: Any) -> "User":
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return self.create_user(email, password=password, **extra_fields)
+from apps.users.managers.user_manager import UserManager
 
 
 class User(UUIDBaseModel, AbstractBaseUser):
