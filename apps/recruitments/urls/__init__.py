512fc25a75992ddb3a7096be6da44a606ab5edbc
Jiwoong0322 <woong4624@gmail.com>
2025-09-09 14:26:29 +0900

    🎨 style: black, isort 코드 포매터 실행
    isort, black, mypy 실행
    
    관련 이슈: #54

--- apps/recruitments/urls/__init__.py
+++ apps/recruitments/urls/__init__.py
@@ -1,5 +1,5 @@
-from apps.recruitments.urls.tags_urls import urlpatterns as tags_urls
 from apps.recruitments.urls.recruitments_urls import urlpatterns as recruitments_urls
+from apps.recruitments.urls.tags_urls import urlpatterns as tags_urls
 
 # app_name = "recruitments"
 
