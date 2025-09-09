4b2482e5b0ea47622d0250a2218e154291e562de
inu9431 <inu9431@naver.com>
2025-09-09 14:59:58 +0900

    💡 chore: 유저 매니저를 managers에 추가

--- apps/users/managers/user_manager.py
+++ apps/users/managers/user_manager.py
@@ -15,3 +15,25 @@ class UserManager(models.Manager["User"]):
     # 관리자 권한 있는 유저 조회
     def staff_users(self) -> models.QuerySet["User"]:
         return self.filter(is_staff=True)
+
+    def create_user(self, email: str, password: str, **extra_fields: Any) -> "User":
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email: str, password: str, **extra_fields: Any) -> "User":
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password=password, **extra_fields)
+
