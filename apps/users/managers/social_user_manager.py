02a555fe92690a5fedddc89d5ab39efb74dc8df3
inu9431 <inu9431@naver.com>
2025-09-09 14:01:52 +0900

    💡 chore    : 타입에러나서 type_checking 임포트 추가

--- apps/users/managers/social_user_manager.py
+++ apps/users/managers/social_user_manager.py
@@ -1,8 +1,12 @@
+from typing import TYPE_CHECKING
 from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 
-class SocialUserManager(models.Manager):
-    def kakao_users(self):
-        return self.filter(provider='kakao')
-    def naver_users(self):
-        return self.filter(provider='naver')
+if TYPE_CHECKING:
+    from apps.users.models.social_user import SocialUser
+class SocialUserManager(models.Manager["SocialUser"]):
+    def kakao_users(self) -> models.QuerySet["SocialUser"]:
+        return self.filter(provider="kakao")
+
+    def naver_users(self) -> models.QuerySet["SocialUser"]:
+        return self.filter(provider="naver")
