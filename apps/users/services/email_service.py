289e4aef9dd6ea8d44910c93402b05ecc0e4c2d1
inu9431 <inu9431@naver.com>
2025-09-09 17:02:47 +0900

    ✨ feat: 캐시 저장을 타입별로 저장되게 수정

---
+++ apps/users/services/email_service.py
@@ -0,0 +1,58 @@
+import uuid
+from smtplib import SMTPException
+
+from django.conf import settings
+from django.core.cache import cache
+from django.core.mail import send_mail
+from rest_framework import status
+from rest_framework.response import Response
+
+from apps.core.utils.base62 import Base62
+
+
+class EmailVerificationService:
+    def generate_verification_code(self) -> str:
+        return Base62.uuid_encode(u=uuid.uuid4())
+
+    def send_verification_email(self, email: str, purpose: str, timeout: int = 3600) -> Response:
+
+        verification_code = self.generate_verification_code()
+        cache_key = f"{purpose}-{email}"
+        cache.set(cache_key, verification_code, timeout=timeout)
+
+        subject_map = {
+            "signup": "회원가입 이메일 인증",
+            "reset_password": "비밀번호 재설정 이메일 인증",
+            "recover_account": "탈퇴 계정 복구 이메일 인증",
+        }
+
+        message_map = {
+            "signup": f"인증 코드를 입력하여 회원가읍을 진행해주세요 {verification_code}",
+            "reset_password": f"비밀번호 재설정을 위해 인증 코드를 입력해주세요 {verification_code}",
+            "recover_account": f"탈퇴 계정 복구를 위해 인증 코드를 입력해주세요 {verification_code}",
+        }
+
+        subject = subject_map[purpose]
+        message = message_map[purpose]
+        from_email = settings.EMAIL_HOST_USER
+        recipient_list = [email]
+
+        try:
+            send_mail(subject, message, from_email, recipient_list)
+            return Response({"detail": f"이메일로 인증번호가 발송되었습니다."}, status=status.HTTP_200_OK)
+        except SMTPException:
+            cache.delete(cache_key)
+            return Response(
+                {"error": "이메일 발송 중 예외가 발생하였습니다."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+    def verify_code(self, email: str, code: str, purpose: str) -> Response:
+
+        cache_key = f"{purpose}-{email}"
+        cache_verification_code = cache.get(cache_key)
+
+        if cache_verification_code != code:
+            return Response({"error": "인증번호가 일치하지 않습니다"}, status=status.HTTP_400_BAD_REQUEST)
+
+        cache.set(cache_key, True, timeout=300)
+        return Response({"detail": f"이메일 인증 성공"}, status=status.HTTP_200_OK)
