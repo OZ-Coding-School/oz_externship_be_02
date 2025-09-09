0d08325fcb1ecbb04822c48679a5fc80045849ed
inu9431 <inu9431@naver.com>
2025-09-09 14:04:50 +0900

    💡 chore    : 타입에러떄문에 type_checking 추가

--- apps/users/managers/user_manager.py
+++ apps/users/managers/user_manager.py
@@ -1,11 +1,14 @@
+from typing import TYPE_CHECKING
 from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 
-class UserManager(models.Manager):
+if TYPE_CHECKING:
+    from apps.users.models.user import User
+class UserManager(models.Manager["User"]):
     # 탈퇴하지않은 유저 조회
-    def active(self):
+    def active(self) -> models.QuerySet["User"]:
         return self.filter(is_active=True)
-    # 관리자 권한 있는 유저 조회
-    def staff_users(self):
-        return self.filter(is_staff=True)
 
+    # 관리자 권한 있는 유저 조회
+    def staff_users(self) -> models.QuerySet["User"]:
+        return self.filter(is_staff=True)
