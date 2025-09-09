4951691b8c5a4f62474558553951084ed6dc6826
inu9431 <inu9431@naver.com>
2025-09-09 15:00:25 +0900

    💡 chore: 상속받는 모델을 uuidbasemodel -> basemodel 수정

--- apps/users/models/withdrawals.py
+++ apps/users/models/withdrawals.py
@@ -18,7 +18,7 @@ class WithdrawalsReasonChoices(models.TextChoices):
     OTHER = "OTHER", "기타"
 
 
-class Withdrawals(UUIDBaseModel):
+class Withdrawals(BaseModel):
     user = models.OneToOneField("users.User", on_delete=models.CASCADE, unique=True, help_text="유저")
     reason = models.CharField(
         max_length=30, choices=WithdrawalsReasonChoices.choices, null=False, blank=False, help_text="탈퇴 사유"
