92408a06a92d7b18d5e9bd92dea3267dc2bfba27
Syapa723 <birdragon2@gmail.com>
2025-09-10 15:23:24 +0900

    💡 chore: 아직 커밋되지 않은 admin user view 주석처리
    from apps.users.views.admin_views 주석처리

--- apps/users/urls/admin_urls.py
+++ apps/users/urls/admin_urls.py
@@ -1,9 +1,9 @@
 from django.urls import URLPattern, URLResolver, path
 
-from apps.users.views.admin_views import UserPermissionAPIView
+# from apps.users.views.admin_views import UserPermissionAPIView
 
 app_name = "admin_user"
 
 urlpatterns: list[URLPattern | URLResolver] = [
-    path("/<uuid:user_uuid>", UserPermissionAPIView.as_view(), name="user_permissions"),
+    # path("/<uuid:user_uuid>", UserPermissionAPIView.as_view(), name="user_permissions"),
 ]
