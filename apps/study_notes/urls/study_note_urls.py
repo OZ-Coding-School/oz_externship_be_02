c15f4f6fff775db02e9859da7c6bc83a3f402837
박준혁 <jun201908@naver.com>
2025-09-10 18:25:01 +0900

    ✨feat : 스터디 기록 생성 API url 작성
    - apps/study_notes/urls/study_note_urls.py 생성 및 작성
    - apps/study_notes/urls/__init__.py 작성
    - config/urls에 include

---
+++ apps/study_notes/urls/study_note_urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from apps.study_notes.views.study_notes_views import StudyNoteCreateView
+
+urlpatterns = [
+    # Post /api/v1/study-notes/<group_uuid> - 스터디 기록 생성
+    path("/<uuid:group_uuid>", StudyNoteCreateView.as_view(), name="create-study-note"),
+]
