7355579487c4069fdf63e11514c83945eb783b13
inu9431 <inu9431@naver.com>
2025-09-11 14:44:12 +0900

    ✨ feat: 계정복구, 비밀번호 재설정 urls 추가

--- apps/users/urls/auth_urls.py
+++ apps/users/urls/auth_urls.py
@@ -1,15 +1,21 @@
-from django.urls import URLPattern, URLResolver, path
+from django.urls import path
 
 from apps.users.views.email_view import (
     SignUpEmailVerificationSendAPIView,
-    SignUpEmailVerifiCationVerifyAPIView,
+    SignUpEmailVerifiCationVerifyAPIView, PasswordResetEmailVerificationSendAPIView,
+    AccountRecoveryEmailVerificationSendAPIView, PassowrdResetEmailVerificationVerifyAPIView,
+    AccountRecoveryEmailVerificationVerifyAPIView
 )
 from apps.users.views.phone_view import SendVerificationCodeAPIView, VerifyCodeAPIView
 from apps.users.views.withdrawals import WithdrawalAPIView
 
-urlpatterns: list[URLPattern | URLResolver] = [
+urlpatterns = [
     path("auth/email/send-code", SignUpEmailVerificationSendAPIView.as_view(), name="email_send_code"),
     path("auth/email/verify", SignUpEmailVerifiCationVerifyAPIView.as_view(), name="email_verify_code"),
+    path("auth/reset-password/send", PasswordResetEmailVerificationSendAPIView.as_view(), name="password_reset_send"),
+    path("auth/reset-password/verify", PassowrdResetEmailVerificationVerifyAPIView.as_view(), name="password_reset_verify"),
+    path("auth/recover-account/sned", AccountRecoveryEmailVerificationSendAPIView.as_view(), name="recover_account_send"),
+    path("auth/recover-account/verify", AccountRecoveryEmailVerificationVerifyAPIView.as_view(), name="recover_account_verify"),
     path("auth/phone/send-code", SendVerificationCodeAPIView.as_view(), name="phone_send_code"),
     path("auth/phone/verify-code", VerifyCodeAPIView.as_view(), name="phone_verify_code"),
     path("auth/withdraw", WithdrawalAPIView.as_view(), name="account_withdrawals"),
