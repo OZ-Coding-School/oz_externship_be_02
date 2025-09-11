a8ef06e8d03d828746c6962f728087bd5ca79f8d
inu9431 <inu9431@naver.com>
2025-09-11 18:41:17 +0900

    💡 chore: 공통 클래스에 테스트 코드 셋업 받아 쓸수있게 수정

--- apps/core/tests/mixins/test_user_mixins.py
+++ apps/core/tests/mixins/test_user_mixins.py
@@ -1,7 +1,14 @@
+from django.urls import reverse
+
 from apps.users.models.user import User
 
 
+class APIClient:
+    pass
+
+
 class TestUserMixin:
+    @classmethod
     def _create_test_user(self, email: str = "test@example.com") -> User:
         return User.objects.create_user(
             email=email,
@@ -12,3 +19,23 @@ class TestUserMixin:
             gender="M",
             birthday="2000-01-01",
         )
+
+    def email_recover_account(self):
+        self.email = "recover@example.com"
+        self.send_url = reverse("recover_account_send")
+        self.verify_url = reverse("recover_account_verify")
+
+    def email_reset_password(self):
+        self.email ="password@example.com"
+        self.send_url = reverse("password_reset_send")
+        self.verify_url = reverse("password_reset_verify")
+
+    def email_verication(self):
+        self.email = "email@example.com"
+        self.send_url = reverse("email_send_code")
+        self.verify_url = reverse("email_verify_code")
+
+    def phone_verification(self):
+        self.client = APIClient()
+        self.send_url = reverse("phone_send_code")
+        self.verify_url = reverse("phone_verify_code")
