9964c19fb9c0e7e4306905e676b4c5b4aac7636d
박준혁 <jun201908@naver.com>
2025-09-11 16:43:13 +0900

    ♻️ refactor : 리팩터링 (urls.py 수정)
    - 앤드포인트 명세서에 맞게 변경

--- apps/study_notes/urls/study_note_urls.py
+++ apps/study_notes/urls/study_note_urls.py
@@ -4,5 +4,5 @@ from apps.study_notes.views.study_notes_views import StudyNoteCreateView
 
 urlpatterns = [
     # Post /api/v1/study-notes/<group_uuid> - 스터디 기록 생성
-    path("/<uuid:group_uuid>", StudyNoteCreateView.as_view(), name="create-study-note"),
+    path("/<uuid:group_uuid>/notes", StudyNoteCreateView.as_view(), name="create-study-note"),
 ]
