702ab11dd3bed50d64b19ae23bbee9d7b4a184f6
inu9431 <inu9431@naver.com>
2025-09-11 14:46:59 +0900

    ✨ feat: 비밀번호 재설정 이메일 인증, 계정 복구 이메일 인증 api추가

--- apps/users/views/email_view.py
+++ apps/users/views/email_view.py
@@ -13,6 +13,9 @@ from apps.users.utils.enums import VerificationPurpose
 
 class SignUpEmailVerificationSendAPIView(APIView):
     permission_classes = (AllowAny,)
+    """
+    회원가입 이메일 코드 전송
+    """
 
     def post(self, request: Request) -> Response:
         serializer = EmailVerificationRequestSerializer(data=request.data)
@@ -26,13 +29,75 @@ class SignUpEmailVerificationSendAPIView(APIView):
 
 class SignUpEmailVerifiCationVerifyAPIView(APIView):
     permission_classes = [AllowAny]
-
+    """
+    회원가입 이메일 전송 코드 검증
+    """
     def post(self, request: Request) -> Response:
         serializer = EmailVerifyCodeSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         email = serializer.validated_data["email"]
         purpose = VerificationPurpose.SIGNUP
-        code = serializer.validated_data["code"]
+        verification_code = serializer.validated_data["verification_code"]
 
-        return EmailVerificationService().verify_code(email=email, purpose=purpose, code=code)
+        return EmailVerificationService().verify_code(email=email, purpose=purpose, verification_code=verification_code)
+
+class PasswordResetEmailVerificationSendAPIView(APIView):
+    permission_classes = [AllowAny]
+    """
+    비밀번호 찾기 이메일 코드 전송 
+    """
+
+    def post(self, request: Request) -> Response:
+        serializer = EmailVerificationRequestSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        email = serializer.validated_data["email"]
+        purpose = VerificationPurpose.RESET_PASSWORD
+
+        return EmailVerificationService().send_verification_email(email=email, purpose=purpose)
+
+class PassowrdResetEmailVerificationVerifyAPIView(APIView):
+    permission_classes = [AllowAny]
+    """
+    비밀번호 찾기 이메일 전송 코드 검증
+    """
+    def post(self, request: Request) -> Response:
+        serializer = EmailVerifyCodeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        email = serializer.validated_data["email"]
+        purpose = VerificationPurpose.RESET_PASSWORD
+        verification_code = serializer.validated_data["verification_code"]
+
+        return EmailVerificationService().verify_code(email=email, purpose=purpose, verification_code=verification_code)
+
+class AccountRecoveryEmailVerificationSendAPIView(APIView):
+    permission_classes = [AllowAny]
+    """
+    계정 복구 이메일  코드 전송
+    """
+    def post(self, request: Request) -> Response:
+        serializer = EmailVerificationRequestSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        email = serializer.validated_data["email"]
+        purpose = VerificationPurpose.RECOVER_ACCOUNT
+
+        return EmailVerificationService().send_verification_email(email=email, purpose=purpose)
+
+class AccountRecoveryEmailVerificationVerifyAPIView(APIView):
+    permission_classes = [AllowAny]
+    """
+    계정 복구 이메일 코드 검증
+    """
+    def post(self, request: Request) -> Response:
+        serializer = EmailVerifyCodeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        print(serializer.errors)
+        email = serializer.validated_data["email"]
+        purpose = VerificationPurpose.RECOVER_ACCOUNT
+        verification_code = serializer.validated_data["verification_code"]
+
+        return EmailVerificationService().verify_code(email=email, purpose=purpose, verification_code=verification_code)
+
