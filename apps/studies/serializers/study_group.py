3959efde87c9a745be29164da70b6ea9a72f842d
KIM SHINEDAY <kimshineday99@gmail.com>
2025-09-11 15:59:12 +0900

    ♻️ refactor: 스터디 그룹 생성 API 피드백 반영
    - Serializer 피드백
        - Nasted Serializer 적용
        - CreateSuccessResponse ModelSerializer로 변경
        - validate 코드 분리
    - User 검증 관련 코드 삭제
    - mock.py 삭제
    - views.py @extend_schema `response`추가

--- apps/studies/serializers/study_group.py
+++ apps/studies/serializers/study_group.py
@@ -1,7 +1,9 @@
 from datetime import datetime, timezone
 from typing import Any, Dict
 
+from django.db import transaction
 from rest_framework import serializers
+from rest_framework.relations import PrimaryKeyRelatedField
 
 from apps.studies.models import GroupMember, StudyGroup
 from apps.users.models.user import User
@@ -9,87 +11,93 @@ from apps.users.models.user import User
 
 class StudyGroupSerializer(serializers.ModelSerializer[StudyGroup]):
     """
-    스터디 그룹 생성 Serializer"""
+    스터디 그룹 Serializer
+    """
 
     class Meta:
         model = StudyGroup
         fields = ["name", "introduction", "max_headcount", "profile_img_url", "start_at", "end_at"]
 
+    def validate_max_headcount(self, value: int) -> int:
+        """
+        스터디 그룹 생성 시, 시작일, 종료일 조건 검증 -> `serializer.is_valid()` 실행 시, 자동으로 검증 함수 호출.
+        **인원 수 검증 로직**
+        :param vslur: 유저가 입력한 스터디 그룹 최대 인원 수
+        """
+        # 인원 수 검증
+        if value > 10:
+            raise serializers.ValidationError("인원 수 초과 되었습니다.")
+        return value
+
     def validate(self, attrs: Dict[str, Any]) -> Dict[str, Any]:
         """
-        스터디 그룹 생성 시, 시작일, 종료일 조건 검증
-        `serializer.is_valid()` 실행 시, 자동으로 검증 함수 호출.
-        * 검증 실패
-            - 종료일이 시작일 이전일 경우
-            - 시작일이 과거일 경우
-            - 스터디 기간이 5일 미만인 경우
-        :param attrs: 유저가 입력한 데이터
-        :return: 검증 완료된 데이터
+        **시작일 / 종료일 관련 검증 로직**
+        - 종료일은 시작일 이후여야 한다.
+        - 시작일은 오늘 / 오늘 이후여야한다.
+        - 스터디 기간은 최소 5일.
+        :param attrs: 유저가 입력한 시작일 / 종료일
         """
-        # 유저 검증 (group_members에 리더 데이터 저장)
-        request = self.context.get("request")
-        if request is None or not hasattr(request, "user"):  # hasattr : 특정 속성을 객체가 가지고 있는지 확인
-            raise serializers.ValidationError("유저 데이터가 존재하지 않습니다.")
-
-        # 인원 수 검증
-        if attrs["max_headcount"] > 10:
-            raise serializers.ValidationError("인원 수 초과 되었습니다.")
-
-            # 날짜 검증
+        # 날짜 검증
         start_at = attrs["start_at"]
         end_at = attrs["end_at"]
 
         # 종료일이 시작일 이전일 경우
         if start_at > end_at:
             raise serializers.ValidationError("스터디 종료일이 시작일보다 이전일 수 없습니다.")
-            # 시작일이 과거일 경우
+        # 시작일이 과거일 경우
         if start_at < datetime.now(tz=timezone.utc):
             raise serializers.ValidationError("스터디 시작일이 과거일 수 없습니다.")
-            # 스터디 기간이 5일 미만인 경우
+        # 스터디 기간이 5일 미만인 경우
         if (end_at - start_at).days < 4:
             raise serializers.ValidationError("스터디 종료 날짜는 시작날 기준 최소 5일 이후여야 합니다. ")
         return attrs
 
-    def create(self, validated_data: Dict[str, Any]) -> StudyGroup:
+
+class StudyCreateSerializer(serializers.ModelSerializer[GroupMember]):
+    """
+    스터디 그룹 멤버 Serializer
+    """
+
+    study_group = StudyGroupSerializer()
+    user: PrimaryKeyRelatedField[User] = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = GroupMember
+        fields = ["study_group", "user", "is_leader"]
+
+    def create(self, validated_data: Dict[str, Any]) -> GroupMember:
         """
         스터디 그룹 생성 시, GroupMember에 리더 멤버 등록
         :param validated_data: study_group 데이터
+        :param user:
         :return:
-        """  # 스터디 그룹 생성
-        study_group = super().create(validated_data)
+        """
+        with transaction.atomic():
+            study_group_data = validated_data.pop("study_group")
+            study_group = StudyGroup.objects.create(**study_group_data)
 
-        # user data 가져오기
-        user = self.context["request"].user  # 시리얼라이저에서 리퀘스트로 유저 데이터 접근
-
-        if not user:
-            raise serializers.ValidationError("유저 데이터가 존재하지 않습니다. 로그인 여부를 확인해주세요.")
-
-            # 리더 멤버 생성
-        GroupMember.objects.create(study_group=study_group, user=user, is_leader=True)
-
-        return study_group
+            leader = GroupMember.objects.create(study_group=study_group, is_leader=True, **validated_data)
+        return leader
 
 
-class StudyGroupLeaderSerializer(serializers.ModelSerializer[User]):
-    """
-    스터디 그룹 리더 데이터 관리하는 Serializer"""
-
+class CreateSuccessResponse(serializers.ModelSerializer[GroupMember]):
     class Meta:
-        model = User
-        fields = ["uuid", "nickname"]
+        # model = StudyGroup
+        # fields = ['uuid', 'name', 'introduction', 'profile_img_url', 'start_at', 'end_at', 'max_headcount', 'created_at']
+        model = GroupMember
+        fields = ["user", "study_group"]
 
-
-class CreateSuccessResponse(serializers.Serializer[Any]):
-    """
-    스터디 그룹 생성 성공시 응답 Serializer
-    """
-
-    uuid = serializers.UUIDField()  # 생성된 스터디 그룹 uuid
-    name = serializers.CharField()  # 스터디 그룹 이름
-    introduction = serializers.CharField()  # 스터디 그룹 소개
-    profile_img_url = serializers.CharField(allow_null=True, allow_blank=True)  # 스터디 그룹 대표 이미지
-    start_at = serializers.DateTimeField()  # 스터디 그룹 시작일
-    end_at = serializers.DateTimeField()  # 스터디 그룹 종료일
-    max_headcount = serializers.IntegerField()  # 스터디 그룹 최대 인원
-    created_by = serializers.JSONField()  # 스터디 그룹 생성한 유저 데이터 (리더)
-    created_at = serializers.DateTimeField()  # 스터디 그룹 생성일
+    def to_representation(self, obj: GroupMember) -> Dict[str, Any]:
+        return {
+            "uuid": str(obj.study_group.uuid),
+            "name": obj.study_group.name,
+            "introduction": obj.study_group.introduction,
+            "profile_img_url": obj.study_group.profile_img_url,
+            "start_at": obj.study_group.start_at.isoformat(),
+            "end_at": obj.study_group.end_at.isoformat(),
+            "max_headcount": obj.study_group.max_headcount,
+            "created_by": {
+                "uuid": str(obj.user.uuid),
+                "nickname": obj.user.nickname,
+            },
+        }
--- apps/studies/tests/test_create_studygroup.py
+++ apps/studies/tests/test_create_studygroup.py
@@ -1,5 +1,5 @@
 import logging
-from datetime import datetime
+from datetime import date, datetime
 from typing import Any, Dict, List
 
 from django.test import TestCase
@@ -7,10 +7,19 @@ from django.urls import reverse
 from rest_framework.test import APIRequestFactory, APITestCase
 
 from apps.studies.models import GroupMember, StudyGroup
-from apps.studies.serializers.study_group import StudyGroupSerializer
-from apps.studies.tests.mock import create_mock_users
+from apps.studies.serializers.study_group import StudyCreateSerializer
+from apps.users.models.user import User
 
 logger = logging.getLogger(__name__)
+
+user_data = {
+    "name": "kimshineday",
+    "nickname": "김빛날",
+    "email": "kimshineday@testemail.com",
+    "phone_number": "01000000000",
+    "gender": "F",
+    "birthday": date(2000, 1, 1),
+}
 
 
 class CreateStudyGroupTestFalse(TestCase):
@@ -24,37 +33,37 @@ class CreateStudyGroupTestFalse(TestCase):
     def setUp(self) -> None:
         self.factory = APIRequestFactory()
         self.request = self.factory.post("/fakefakefake/")
-        self.request.user = create_mock_users(1)[0]
+        self.request.user = User.objects.create(**user_data)
 
     def test_create_fail(self) -> None:
         fail_data: List[Dict[str, Any]] = [
-            {"create_data": {}, "context": {}},  # 깡통 데이터
+            {"study_group": {}},  # 깡통 데이터
             {
-                "create_data": {
+                "study_group": {  # 스터디 기간 조건 X
                     "name": "테스트",
                     "introduction": "스터디그룹 생성 테스트 진행 중",
                     "max_headcount": 4,
                     "profile_img_url": "http://imgimgimgimg.png",
                     "start_at": datetime(2025, 9, 28),
-                    # 스터디 기간 조건 X                    "end_at": datetime(2025, 9, 30),
-                },
-                "context": {"request": self.request},
-            },
-            {
-                "create_data": {
-                    "name": "리더생성 테스트",
-                    "introduction": "실패 테스트.",
-                    "max_headcount": 3,
-                    "start_at": datetime(2025, 9, 15),
                     "end_at": datetime(2025, 9, 30),
-                },
-                "context": {},
+                }
             },
+            # {
+            #     "create_data": {
+            #         "study_group": {
+            #             "name": "리더생성 테스트",
+            #             "introduction": "실패 테스트.",
+            #             "max_headcount": 3,
+            #             "start_at": datetime(2025, 9, 15),
+            #             "end_at": datetime(2025, 9, 30),
+            #         }
+            #     },
+            # },
         ]
 
         for case in fail_data:
-            serializer = StudyGroupSerializer(data=case["create_data"], context=case["context"])
-            self.assertFalse(serializer.is_valid())  # 시리얼라이저를 통해 데이터 검증 결과 출력
+            serializer = StudyCreateSerializer(data=case["study_group"])
+            serializer.is_valid(raise_exception=False)
 
 
 class CreateStudyGroupTestSuccess(TestCase):
@@ -67,55 +76,60 @@ class CreateStudyGroupTestSuccess(TestCase):
     def setUp(self) -> None:
         """
         가상 user data 생성
-        """  # 스터디 그룹 생성 시, 생성자를 그룹 리더로 저장하기 위해 시리얼라이저에서 그룹 데이터 저장 후에 멤버 저장 로직까지 구현완료
+        """
+        # 스터디 그룹 생성 시, 생성자를 그룹 리더로 저장하기 위해 시리얼라이저에서 그룹 데이터 저장 후에 멤버 저장 로직까지 구현완료
         # serializer 에서 유저 데이터를 request.user로 가져오는데, 이를 위해서 테스트용 request 객체 생성하는 APIRequestFactory 모듈을 사용함.
         self.factory = APIRequestFactory()
         self.request = self.factory.post("/fakefakefake/")
-        self.request.user = create_mock_users(1)[0]
+        self.request.user = User.objects.create(**user_data)
 
     def test_create_success(self) -> None:  # 테스트 성공, 이미지 url 생략
         success_data = [
             {
                 "create_data": {
-                    "name": "테스트 성공기원 1",
-                    "introduction": "이미지 필수 아니여서 제외.",
-                    "max_headcount": 3,
-                    "start_at": datetime(2025, 9, 15),
-                    "end_at": datetime(2025, 9, 30),
+                    "study_group": {
+                        "name": "테스트 성공기원 1",
+                        "introduction": "이미지 필수 아니여서 제외.",
+                        "max_headcount": 3,
+                        "start_at": datetime(2025, 9, 15),
+                        "end_at": datetime(2025, 9, 30),
+                    }
                 },
-                "context": {"request": self.request},
+                # "context": {"request": self.request},
             },
             {
                 "create_data": {
-                    "name": "테스트 성공기원 2",
-                    "introduction": "스터디 그룹 프로필 이미지 추가.",
-                    "max_headcount": 3,
-                    "profile_img_url": "http://imgimgimgimg.png",  # 이미지 값 추가
-                    "start_at": datetime(2025, 9, 15),
-                    "end_at": datetime(2025, 9, 30),
+                    "study_group": {
+                        "name": "테스트 성공기원 2",
+                        "introduction": "스터디 그룹 프로필 이미지 추가.",
+                        "max_headcount": 3,
+                        "profile_img_url": "http://imgimgimgimg.png",  # 이미지 값 추가
+                        "start_at": datetime(2025, 9, 15),
+                        "end_at": datetime(2025, 9, 30),
+                    }
                 },
-                "context": {"request": self.request},
+                # "context": {"request": self.request},
             },
         ]
 
         for case in success_data:
-            serializer = StudyGroupSerializer(data=case["create_data"], context={"request": self.request})
+            serializer = StudyCreateSerializer(
+                data=case["create_data"],
+            )
             self.assertTrue(serializer.is_valid())
 
             # 데이터 객체 생성
-            serializer.save()
+            check = serializer.save(user=self.request.user)
 
     def tearDown(self) -> None:
         """
         test db에 저장된 데이터 확인
-        :return:  data
+        :return: data
         """
         all_groups = StudyGroup.objects.all().values()
         all_members = GroupMember.objects.all().values()
         logger.debug(f"Groups: {all_groups}")
         logger.debug(f"Members: {all_members}")
-
-    # API TEST
 
 
 class CreateStudyGroupAPITestFail(APITestCase):
@@ -128,17 +142,18 @@ class CreateStudyGroupAPITestFail(APITestCase):
         가상 user data 생성
         """
         self.url = reverse("studies:create_study_group")  # 라우터 설정
-        self.mock_user = create_mock_users(1)[0]  # 가상 유저 데이터 생성
-        self.client.force_authenticate(user=self.mock_user)  # 유저 로그인
+        self.user = User.objects.create(**user_data)  # 가상 유저 데이터 생성
+        self.client.force_authenticate(user=self.user)  # 유저 로그인
 
     def test_post_fail(self) -> None:
         """
         스터디 그룹 작성 API, post 요청 테스트
-        결과 값 400        :return:
+        결과 값 400
+        :return:
         """
         fail_data = [
             {
-                "create_data": {
+                "study_group": {
                     "name": "API 실패 테스트 1",
                     "introduction": "인원 초과",
                     "max_headcount": 11,
@@ -147,27 +162,27 @@ class CreateStudyGroupAPITestFail(APITestCase):
                 }
             },
             {
-                "create_data": {
+                "study_group": {
                     "name": "API 실패 테스트 2",
-                    "introduction": "일정 설정 오류",
+                    "introduction": "일정 설정 오류 - 시작날이 오늘 / 오늘 이후",
                     "max_headcount": 6,
                     "start_at": "2025-09-01T00:00:00Z",
                     "end_at": "2025-09-30T00:00:00Z",
                 }
             },
             {
-                "create_data": {
-                    "name": "API 실패 테스트 2",
-                    "introduction": "일정 설정 오류",
+                "study_group": {
+                    "name": "API 실패 테스트 3",
+                    "introduction": "일정 설정 오류 - 끝나는 날짜가 시작날보다 이전",
                     "max_headcount": 6,
                     "start_at": "2025-09-30T00:00:00Z",
                     "end_at": "2025-09-15T00:00:00Z",
                 }
             },
             {
-                "create_data": {
-                    "name": "API 실패 테스트 2",
-                    "introduction": "일정 설정 오류",
+                "study_group": {
+                    "name": "API 실패 테스트 4",
+                    "introduction": "일정 설정 오류 - 스터디는 최소 5일 진행",
                     "max_headcount": 6,
                     "start_at": "2025-10-01T00:00:00Z",
                     "end_at": "2025-10-03T00:00:00Z",
@@ -176,7 +191,8 @@ class CreateStudyGroupAPITestFail(APITestCase):
         ]
 
         for case in fail_data:
-            response = self.client.post(self.url, data=case["create_data"], format="json")
+            response = self.client.post(self.url, data=case, format="json")
+            self.assertEqual(response.status_code, 400)
 
 
 class CreateStudyGroupAPITest(APITestCase):
@@ -186,31 +202,31 @@ class CreateStudyGroupAPITest(APITestCase):
 
     def setUp(self) -> None:
         self.url = reverse("studies:create_study_group")  # 라우터 설정
-        self.mock_user = create_mock_users(1)[0]  # 가상 유저 데이터 생성
-        self.client.force_authenticate(user=self.mock_user)  # 유저 로그인
+        self.user = User.objects.create(**user_data)  # 가상 유저 데이터 생성
+        self.client.force_authenticate(user=self.user)  # 유저 로그인
 
     def test_post_success(self) -> None:
         """
         스터디 그룹 작성 API, post 요청 테스트
-        결과 값 201        :return:
+        결과 값 201
+        :return:
         """
-        create_data = {
-            "name": "API 테스트 1",
-            "introduction": "post 요청 성공해야지",
-            "max_headcount": 4,
-            "start_at": "2025-09-15T00:00:00Z",
-            "end_at": "2025-09-30T00:00:00Z",
+        success_data = {
+            "study_group": {
+                "name": "Python 개념 잡기",
+                "introduction": "Python 언어 기초를 공부, 프로그래머스 문제 풀기.",
+                "max_headcount": 5,
+                "profile_img_url": "http://imgimgimgimg.png",  # 이미지 값 추가
+                "start_at": "2025-09-15T00:00:00Z",
+                "end_at": "2025-09-30T00:00:00Z",
+            }
         }
 
-        response = self.client.post(self.url, data=create_data, format="json")
+        response = self.client.post(self.url, data=success_data, format="json")
 
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.data["name"], "API 테스트 1")
+        self.assertEqual(response.data["name"], "Python 개념 잡기")
         self.assertEqual(StudyGroup.objects.count(), 1)
-        logger.debug("status_code: %s", response.status_code)
-        logger.debug("response data: %s", response.data)
-        logger.error("status_code: %s", response.status_code)
-        logger.error("response data: %s", response.data)
 
     def tearDown(self) -> None:
         """
