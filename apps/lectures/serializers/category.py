418e1f5ec2b1f31de8563324123adb14c2cdfb12
park3hho <ejfhr7777@gmail.com>
2025-09-12 11:45:14 +0900

    💡 chore: CICD 수정

--- apps/lectures/serializers/category.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-
-from apps.lectures.models.categories import Category
-
-
-class CategorySerializer(ModelSerializer[Category]):
-    class Meta:
-        model = Category
-        fields = ["name"]
