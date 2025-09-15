8aa605a606e7b9b2d96cf733b4940c94f29e1b06
Dayeon <0812dayeon@gmail.com>
2025-09-15 16:11:29 +0900

    chat serializer 구현

---
+++ apps/chat/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import ChatMessage
+from apps.studies.models import StudyGroup # study groups 이름을 가지고 오기 위해 import
+from apps.users.models import User # user의 이름을 가지고 오기 위해서 import 했음
+
+class ChatMessageSerializer(serializers.ModelSerializer[ChatMessage]):
+    study_group_name = serializers.SerializerMethodField(read_only=True) # study group 이름
+    # last_message = serializers.SerializerMethodField(read_only=True) # 가장 최근에 온 메세지(content) # 상황에 따라서 view에서 처리 (N+1 문제 방지)
+    is_read = serializers.SerializerMethodField(read_only=True) # 읽음 여부
+
+    class Meta:
+        model = ChatMessage
+        fields = ["study_group_name", "updated_at", "created_at", "is_read", "content"]
--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.db import models
+from rest_framework.views import APIView
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from .models import ChatMessage
+from .serializers import ChatMessageSerializer
+from django.shortcuts import get_object_or_404
+from apps.users import serializers
+from django.contrib.auth.models import User
 
-# Create your views here.
+class ChatRoomListView(models.Model):
+
+    # 채팅방 목록 조회
+    def get(self, request):
+        user = request.user
+        queryset = User.objects.all()
+        serializer = User.objects.all()
+        return Response(serializer.data)
+
+        def get_queryset(self):
+            # 유저가 속한 그터디 그룹의 메세지만!
+            return
+
