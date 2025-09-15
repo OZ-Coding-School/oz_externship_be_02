5adc0d1f7944a0238a26edff7add6f17473a4120
c0z-y-fiction <cosm0snnt@gmail.com>
2025-09-15 18:28:55 +0900

    ✨ feat: recover_account() 개선
    탈퇴 요청이 없는 계정이 복구를 시도하려고 하면 None 대신 ObjectDoesNotExist 예외를 발생시키도록 변경(추후 오류 처리를 위해), view로 코드 이전

--- apps/users/services/withdrawals.py
+++ apps/users/services/withdrawals.py
@@ -4,7 +4,7 @@ from datetime import date, timedelta
 
 from apps.users.models.user import User
 from apps.users.models.withdrawals import Withdrawals
-from apps.users.services.email_service import EmailVerificationService
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def create_withdrawal(user: User, reason: str, reason_detail: str) -> Withdrawals:
@@ -18,39 +18,29 @@ def create_withdrawal(user: User, reason: str, reason_detail: str) -> Withdrawal
 
     # 비즈니스 로직 2: 모델 객체 생성(탈퇴 요청 생성)
     # withdrawal 모델에 탈퇴 요청 저장
-    # user.is_active는 views에서 처리(validate에서 처리하지 않는 이유: user가 비활성화된 상태일 수도 있기 때문) < 이거 자꾸 자동 완성되는데 뭐지
     withdrawal = Withdrawals.objects.create(user=user, reason=reason, reason_detail=reason_detail, due_date=due_date)
 
     return withdrawal
 
 
-def recover_account(email: str, code: str) -> User | None:
+def recover_account(email: str, verification_code: str) -> User | None:
     """
     인증 코드 검증 후 유저 계정을 복구하고 탈퇴 요청을 삭제.
     """
 
     # 탈퇴 요청한 유저 조회
-    withdrawal = Withdrawals.objects.select_related("user").filter(user__email=email).first()
+    withdrawal = Withdrawals.objects.select_related("user").filter(user_email=email).first()
 
-    # 탈퇴 요청이 없다면 None 반환
+    # 탈퇴 요청이 없다면 Error 반환: 추후 오류 처리를 위한 ObjectDoesNotExist 예외 사용
     if not withdrawal:
-        return None
-
-    user = withdrawal.user
-
-    # 인증 코드 검증
-    email_service = EmailVerificationService()
-    verification_result = email_service.verify_code(email=email, code=code, purpose="recover_account")
-
-    # 인증 코드가 유효하지 않다면 None 반환
-    if verification_result.status_code != 200:
-        return None
+        raise ObjectDoesNotExist("해당 이메일로 탈퇴 요청이 존재하지 않습니다.")
 
     # 유저 계정 복구 및 탈퇴 요청 삭제
     # 탈퇴 요청만 삭제하는 거고 user 모델의 상태는 변경하지 않음
     # 따라서 user.is_active = True를 명시적으로 설정
+    user = withdrawal.user
     user.is_active = True
     user.save()
-    withdrawal.delete()
+    # withdrawal.delete()
 
     return user
