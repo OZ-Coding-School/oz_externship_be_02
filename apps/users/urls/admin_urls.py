a1566a580a8c38a0c91d069117b8646280889ce4
Syapa723 <birdragon2@gmail.com>
2025-09-16 18:04:54 +0900

    ✨ feat: 회원 관리 페이지 조회 API 개발
    회원 관리 페이지 조회 기능 serializer, view, url 작성
    django-filter-stubs 의존성 패키지 설치

--- apps/users/urls/admin_urls.py
+++ apps/users/urls/admin_urls.py
@@ -1,9 +1,14 @@
-from django.urls import URLPattern, URLResolver, path
+from django.urls import URLPattern, URLResolver, include, path
+from rest_framework.routers import DefaultRouter
 
-from apps.users.views.admin_views import UserPermissionUpdateAPIView
+from apps.users.views.admin_views import UserAdminViewSet, UserPermissionUpdateAPIView
 
 app_name = "admin_user"
 
+router = DefaultRouter()
+router.register("users", UserAdminViewSet, basename="users")
+
 urlpatterns: list[URLPattern | URLResolver] = [
-    path("/<uuid:user_uuid>/permission", UserPermissionUpdateAPIView.as_view(), name="user_permissions"),
+    path("users/<uuid:user_uuid>/permission", UserPermissionUpdateAPIView.as_view(), name="user_permissions"),
+    path("", include(router.urls)),
 ]
