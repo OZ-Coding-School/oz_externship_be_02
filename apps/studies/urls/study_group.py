c574f3fe8d47b3e13570fa8706a137bcc6422375
meoyoug <eodyd426@gmail.com>
2025-09-16 21:23:13 +0900

    🐛 fix: apps/studies/urls/study_group.py 에 임포트문 오류 수정

--- apps/studies/urls/study_group.py
+++ apps/studies/urls/study_group.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from apps.studies.views.create_studygroup import CreateStudyGroupView
+from apps.studies.views.study_group_create_view import CreateStudyGroupView
 
 urlpatterns = [
     path("", CreateStudyGroupView.as_view(), name="create_study_group"),
