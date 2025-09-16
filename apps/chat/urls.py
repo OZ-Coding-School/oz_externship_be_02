65272a5cf754ce6fcd7bac155bf829b10b699908
Dayeon <0812dayeon@gmail.com>
2025-09-16 20:29:46 +0900

    chat urls 연결

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -1,7 +1,10 @@
 from rest_framework.routers import DefaultRouter
 from .views import ChatRoomListView
+from django.urls import path
 
 router = DefaultRouter()
 router.register('messages', ChatRoomListView, basename='chatroom')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("", ChatRoomListView.as_view(), name='chatroomlist'),
+]
