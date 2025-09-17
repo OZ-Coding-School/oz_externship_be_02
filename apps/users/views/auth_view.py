bc3d6c55eefd67cb58420f9fbbfabe2e56bf58a8
inu9431 <inu9431@naver.com>
2025-09-17 18:54:55 +0900

    💡 chore: 불필요한 로직 제거

--- apps/users/views/auth_view.py
+++ apps/users/views/auth_view.py
@@ -9,8 +9,6 @@ from apps.users.services.exceptions import (
     EmailVerificationCodeFailedError,
     PhoneVerificationCodeFailedError,
 )
-from apps.users.services.signup_service import UserSignUpService
-
 
 class UserSignupAPIView(APIView):
     permission_classes = (AllowAny,)
@@ -18,16 +16,6 @@ class UserSignupAPIView(APIView):
     def post(self, request: Request) -> Response:
         serializer = UserSignupSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-        signup_data = serializer.validated_data.copy()
-        signup_data.pop("email_verification_code", None)
-        signup_data.pop("phone_verification_code", None)
-
-        try:
-            user = UserSignUpService.signup(**signup_data)
-        except EmailVerificationCodeFailedError as e:
-            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-        except PhoneVerificationCodeFailedError as e:
-            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
-        return Response({"detail": "회원가입이 완료되었습니다", "user_id": user.uuid}, status=status.HTTP_201_CREATED)
+        return Response({"detail": "회원가입이 완료되었습니다", "user": serializer.data}, status=status.HTTP_201_CREATED)
