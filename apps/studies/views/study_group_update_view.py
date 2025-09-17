d69318e44419b94ee943584143ecffffb26e0efe
KIM SHINEDAY <kimshineday99@gmail.com>
2025-09-17 18:53:57 +0900

    ✨ feat: 스터디 그룹 정보 수정 API 개발
    - urls.py 라우터 추가
    - UpdateStudyGroupView 작성

---
+++ apps/studies/views/study_group_update_view.py
@@ -0,0 +1,33 @@
+import logging
+
+from django.shortcuts import get_object_or_404
+from drf_spectacular.utils import extend_schema
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from apps.studies.models import StudyGroup
+from apps.studies.serializers.study_group import StudyGroupCreateSerializer
+
+logger = logging.getLogger(__name__)
+
+
+class UpdateStudyGroupView(APIView):
+    """
+    스터디 그룹 정보 수정 APIView
+    """
+
+    @extend_schema(
+        summary="스터디 그룹 정보 수정 API",
+        description="스터디 그룹의 리더는 스터디 그룹 상세 조회 페이지 내에서 스터디 그룹 정보 수정페이지로 이동하여 그룹 정보를 수정할 수 있습니다.",
+        request=StudyGroupCreateSerializer,
+        tags=["Study Group"],
+        responses=StudyGroupCreateSerializer,
+    )
+    def patch(self, request: Request, group_uuid: str) -> Response:
+        study_group_data = get_object_or_404(StudyGroup, uuid=group_uuid)
+        serializer = StudyGroupCreateSerializer(instance=study_group_data, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
