738a8ed0228fb9419a45b9338b67dfe5d8e13f99
KIM SHINEDAY <kimshineday99@gmail.com>
2025-09-18 12:05:17 +0900

    💡 chore: 주석 추가 및 코드 포매터 적용 변경사항 커밋
    - Serializer 주석 추가 및 수정
    - 코드 포맷터 적용, 변경사항 저장

--- apps/studies/serializers/study_group.py
+++ apps/studies/serializers/study_group.py
@@ -78,25 +78,26 @@ class StudyGroupCreateSerializer(serializers.ModelSerializer[StudyGroup]):
         """
         스터디 그룹 생성과 함께 GroupMember에 리더 멤버 등록
         :param validated_data: study_group 데이터
-        :param user:
         :return: study_group data
         """
-        user = validated_data.pop("user")
+        user = validated_data.pop("user")  # 유저 데이터만 추출
 
-        lectures = validated_data.pop("lectures")
+        lectures = validated_data.pop("lectures")  # 선택 강의 데이터만 추출
 
-        if "profile_img" in validated_data:
-            img = validated_data.pop("profile_img")
-            s3_uploader = S3Uploader()
-            profile_img_url = s3_uploader.upload_file(file=img)
-            validated_data["profile_img_url"] = profile_img_url.get("url")
+        if "profile_img" in validated_data:  # 이미지 데이터가 있을 경우에만 실행
+            img = validated_data.pop("profile_img")  # 이미지 데이터 추출
+            s3_uploader = S3Uploader()  # S3 업로드 객체화
+            profile_img_url = s3_uploader.upload_file(file=img)  # S3에 이미지 업로드
+            validated_data["profile_img_url"] = profile_img_url.get("url")  # profile img url 저장
 
-        with transaction.atomic():
-            study_group = StudyGroup.objects.create(**validated_data)
-            study_lectures = [StudyLecture(lecture=i, study_group=study_group) for i in lectures]
-            StudyLecture.objects.bulk_create(study_lectures)
-            GroupMember.objects.create(study_group=study_group, is_leader=True, user=user)
-            study_group.refresh_from_db()
+        with transaction.atomic():  # 하나의 작업으로 묶음, 실패 시 롤백
+            study_group = StudyGroup.objects.create(**validated_data)  # 스터디 그룹 관련 데이터들을 생성.
+            study_lectures = [
+                StudyLecture(lecture=i, study_group=study_group) for i in lectures
+            ]  # 선택 강의 데이터, Many to Many 연결용 객체 리스트
+            StudyLecture.objects.bulk_create(study_lectures)  # bulk insert로 중간 테이블에 데이터 저장
+            GroupMember.objects.create(study_group=study_group, is_leader=True, user=user)  # 그룹 멤버에 leader로 저장.
+            study_group.refresh_from_db()  # DB 객체 캐시 동기화, bulk_create 추가된 Many to Many 반영
         return study_group
 
     def update(self, instance: StudyGroup, validated_data: Dict[str, Any]) -> Any:
@@ -106,7 +107,7 @@ class StudyGroupCreateSerializer(serializers.ModelSerializer[StudyGroup]):
         :param validated_data: 검증 끝난 데이터
         :return:
         """
-        if "profile_img" in validated_data:
+        if "profile_img" in validated_data:  # 이미지 데이터가 있을 경우에만 실행
             img = validated_data.pop("profile_img")
             s3_uploader = S3Uploader()
             profile_img_url = s3_uploader.upload_file(file=img)
