8ea05f20472db45a678e8516ec801834e08a4117
yoo-sh-96 <tmdguq90051@gmail.com>
2025-09-18 19:52:36 +0900

    💡 chore: 리시버 서비스 호출 변경
    서비스 함수명 변경에 따른 리시버 서비스 호출 변경
    
    관련 이슈: #116

--- apps/notifications/receivers.py
+++ apps/notifications/receivers.py
@@ -29,7 +29,7 @@ def notify_add_application(instance: Application, created: bool, **kwargs: Any) 
     app_id = instance.pk
 
     def add_application() -> None:
-        ApplicationNotificationService.from_id(app_id).notification_add_application()
+        ApplicationNotificationService.from_id(app_id).notify_add_application()
 
     transaction.on_commit(add_application)
 
@@ -49,10 +49,10 @@ def notify_status_change_to_applicant(instance: Application, created: bool, **kw
     if curr == Application.ApplicationStatus.ACCEPTED:  # 현재가 승인  상태로 바뀐 경우만
 
         def accept_application() -> None:
-            ApplicationNotificationService.from_id(app_id).notification_application_accept()
+            ApplicationNotificationService.from_id(app_id).notify_application_accept()
 
         def join_study_group() -> None:
-            ApplicationNotificationService.from_id(app_id).notification_group_members_join()
+            ApplicationNotificationService.from_id(app_id).notify_group_members_join()
 
         transaction.on_commit(accept_application)
         transaction.on_commit(join_study_group)
@@ -60,6 +60,6 @@ def notify_status_change_to_applicant(instance: Application, created: bool, **kw
     if curr == Application.ApplicationStatus.REJECTED:
 
         def reject_application() -> None:
-            ApplicationNotificationService.from_id(app_id).notification_application_reject()
+            ApplicationNotificationService.from_id(app_id).notify_application_reject()
 
         transaction.on_commit(reject_application)
