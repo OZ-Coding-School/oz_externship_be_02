e7fc22bf512b518564efe1ca9034850f4404cbef
최재현 <zip020314@gmail.com>
2025-09-19 18:03:39 +0900

    ♻️ refactor:디버그 조건 제거 및 이미지삭제 트랜잭션 수정

--- apps/recruitments/tests/test_attachments.py
+++ apps/recruitments/tests/test_attachments.py
@@ -1,16 +1,15 @@
 from datetime import date
 from typing import Any
-
+from unittest.mock import patch
 from django.core.files.uploadedfile import SimpleUploadedFile
-from django.test import override_settings
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITransactionTestCase
 
 from apps.users.models.user import User
 
 
-class RecruitmentFileUploadViewTest(APITestCase):
+class RecruitmentFileUploadViewTest(APITransactionTestCase):
 
     def setUp(self) -> None:
         self.user = User.objects.create_user(
@@ -19,9 +18,13 @@ class RecruitmentFileUploadViewTest(APITestCase):
         self.client.force_authenticate(user=self.user)
         self.url = reverse("recruitment-file-upload")
 
-    @override_settings(DEBUG=True)
-    def test_file_upload_success(self) -> None:
+    @patch("apps.recruitments.views.attachments_views.S3Uploader")
+    def test_file_upload_success(self, mock_s3_uploader_class:Any) -> None:
         # GIVEN
+        mock_s3_instance = mock_s3_uploader_class.return_value
+        mock_s3_instance.upload_file.return_value = {
+            "url": "https://s3.mock-domain.com/attachments/test.pdf"
+        }
         test_file = SimpleUploadedFile("test.pdf", b"file_content", content_type="application/pdf")
         data = {"file": test_file}
 
