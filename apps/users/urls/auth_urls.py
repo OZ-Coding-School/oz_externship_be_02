7047c98b4fddd1571972d67c2a36e56bc9ed4654
inu9431 <inu9431@naver.com>
2025-09-19 15:20:55 +0900

    💡 chore: develop이랑 signup브런치 이름달라서 충돌나서 해결

--- apps/users/urls/auth_urls.py
+++ apps/users/urls/auth_urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from apps.users.views.email_view import (
+from apps.users.views.email_verification_view  import (
     AccountRecoveryEmailVerificationSendAPIView,
     AccountRecoveryEmailVerificationVerifyAPIView,
     PassowrdResetEmailVerificationVerifyAPIView,
@@ -8,8 +8,9 @@ from apps.users.views.email_view import (
     SignUpEmailVerificationSendAPIView,
     SignUpEmailVerifiCationVerifyAPIView,
 )
-from apps.users.views.phone_view import SendVerificationCodeAPIView, VerifyCodeAPIView
-from apps.users.views.withdrawals_view import AccountRecoveryAPIView, WithdrawalAPIView
+from apps.users.views.phone_verification_view import SendVerificationCodeAPIView, VerifyCodeAPIView
+from apps.users.views.auth_view import UserSignupAPIView
+from apps.users.views.withdrawals_view import WithdrawalAPIView
 
 urlpatterns = [
     path("auth/email/send-code", SignUpEmailVerificationSendAPIView.as_view(), name="email_send_code"),
@@ -29,7 +30,7 @@ urlpatterns = [
         name="recover_account_verify",
     ),
     path("auth/phone/send-code", SendVerificationCodeAPIView.as_view(), name="phone_send_code"),
-    path("auth/phone/verify-code", VerifyCodeAPIView.as_view(), name="phone_verify_code"),
+    path("auth/phone/verify", VerifyCodeAPIView.as_view(), name="phone_verify_code"),
     path("auth/withdraw", WithdrawalAPIView.as_view(), name="account_withdrawals"),
-    path("auth/recover", AccountRecoveryAPIView.as_view(), name="account_recovery"),
+    path("auth/email/signup", UserSignupAPIView.as_view(), name="signup"),
 ]
