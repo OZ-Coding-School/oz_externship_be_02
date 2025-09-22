700e35545ecf77e90d79661e0c976332460d2fda
BE-11-dongjae <p970626@gmail.com>
2025-09-22 11:09:10 +0900

    💡 chore : 배포환경에서도 적용시키기 위해 local 내용을 base로 옮김

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,5 +1,3 @@
-import os
-
 from config.settings.base import *
 
 DEBUG = True
@@ -21,6 +19,3 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
-
-KAKAO_CLIENT_ID = os.getenv("KAKAO_CLIENT_ID")
-KAKAO_REDIRECT_URI = os.getenv("KAKAO_REDIRECT_URI")
