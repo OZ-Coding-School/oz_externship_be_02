766e817c5293d81b4d94515034f86e9dabf440b2
최재현 <zip020314@gmail.com>
2025-09-22 13:48:44 +0900

    💡 chore: 파일명 변경 recruitments_views.py -> recruitments_detail_views.py
    파일명 변경 apps/recruitments/views/recruitments_views.py -> apps/recruitments/views/recruitments_detail_views.py

--- apps/recruitments/urls/recruitments_urls.py
+++ apps/recruitments/urls/recruitments_urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from apps.recruitments.views.attachments_views import RecruitmentFileView
 from apps.recruitments.views.images_views import RecruitmentImageView
 from apps.recruitments.views.recruitment_views import MyRecruitmentView, RecruitmentView
-from apps.recruitments.views.recruitments_views import RecruitmentDetailView
+from apps.recruitments.views.recruitment_detail_views import RecruitmentDetailView
 
 urlpatterns = [
     path("", RecruitmentView.as_view(), name="recruitment-list"),
