e9e8e4f7a2265a1d44d7eb685f86bf4f0ffdf0c0
c0z-y-fiction <cosm0snnt@gmail.com>
2025-09-22 22:27:20 +0900

    ✨ feat: 사용자 조회 및 수정 용 API(serializer.py) 추가
    - UserInfoSerializer: 로그인한 사용자의 기본 정보를 조회.
    - UserInfoEditSerializer: 로그인한 사용자의 기본 정보를 수정.
    - 비밀번호는 암호화하여 저장.

--- apps/users/serializers/user_info_serializer.py
+++ apps/users/serializers/user_info_serializer.py
@@ -7,12 +7,12 @@ from apps.users.models.user import User
 
 # 유저 정보 조회용
 class UserInfoSerializer(serializers.ModelSerializer[User]):
-    profile_img_url: serializers.URLField
-    email: serializers.EmailField
-    nickname: serializers.CharField
-    name: serializers.CharField
-    phone_number: serializers.CharField
-    birthday: serializers.DateField
+    profile_img_url = serializers.URLField()
+    email = serializers.EmailField()
+    nickname = serializers.CharField()
+    name = serializers.CharField()
+    phone_number = serializers.CharField()
+    birthday = serializers.DateField()
 
     class Meta:
         model = User
@@ -27,11 +27,14 @@ class UserInfoSerializer(serializers.ModelSerializer[User]):
 
 
 # 유저 정보 수정용
+# write_only: 서버에 전달만 하고 응답에는 포함하지 않음
 class UserInfoEditSerializer(serializers.ModelSerializer[User]):
-    profile_img: serializers.URLField
-    password: serializers.CharField
-    nickname: serializers.CharField
-    phone_number: serializers.CharField
+    profile_img_url = serializers.URLField(required=False)
+    password = serializers.CharField(required=False, write_only=True)
+    nickname = serializers.CharField(required=False)
+    phone_number = serializers.CharField(required=False)
+    is_phone_number_verified = serializers.BooleanField(required=False, write_only=True)
+    # True: 인증 완료 / False: 인증 미완료(인증 필요)
 
     class Meta:
         model = User
@@ -40,4 +43,12 @@ class UserInfoEditSerializer(serializers.ModelSerializer[User]):
             "password",
             "nickname",
             "phone_number",
+            "is_phone_number_verified",
         ]
+
+    # 비밀번호 암호화
+    def update(self, instance: User, validated_data: dict) -> User:
+        password = validated_data.pop("password", None)
+        if password:
+            instance.set_password(password)
+        return super().update(instance, validated_data)
