f268a5a5f4b246c32483644e3e8bacf8113ac083
c0z-y-fiction <cosm0snnt@gmail.com>
2025-09-22 23:48:14 +0900

    💡 chore: is_phone_number_verified 옵션 변경(write_only → read_only)

--- apps/users/serializers/user_info_serializer.py
+++ apps/users/serializers/user_info_serializer.py
@@ -27,14 +27,12 @@ class UserInfoSerializer(serializers.ModelSerializer[User]):
 
 
 # 유저 정보 수정용
-# write_only: 서버에 전달만 하고 응답에는 포함하지 않음
 class UserInfoEditSerializer(serializers.ModelSerializer[User]):
     profile_img_url = serializers.URLField(required=False)
     password = serializers.CharField(required=False, write_only=True)
     nickname = serializers.CharField(required=False)
     phone_number = serializers.CharField(required=False)
-    is_phone_number_verified = serializers.BooleanField(required=False, write_only=True)
-    # True: 인증 완료 / False: 인증 미완료(인증 필요)
+    is_phone_number_verified = serializers.BooleanField(required=False, read_only=True)
 
     class Meta:
         model = User
