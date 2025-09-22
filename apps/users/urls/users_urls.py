b980b0ce4587689de8895cb6be00e2971b87d9f0
c0z-y-fiction <cosm0snnt@gmail.com>
2025-09-22 22:08:34 +0900

    ✨ feat: 회원 정보 조회 및 수정 URL 추가(user_info)

--- apps/users/urls/users_urls.py
+++ apps/users/urls/users_urls.py
@@ -1,3 +1,6 @@
 from django.urls import URLPattern, URLResolver, path
+from apps.users.views.user_info_view import UserInfoView
 
-urlpatterns: list[URLPattern | URLResolver] = []
+urlpatterns: list[URLPattern | URLResolver] = [
+    path("info/", UserInfoView.as_view(), name="user_info"),
+]
