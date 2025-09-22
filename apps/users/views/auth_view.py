45280aa9475aee7b56f05ed3e1e3bee513c1f471
inu9431 <inu9431@naver.com>
2025-09-22 14:39:23 +0900

    🎨 style    : mypy 해결

--- apps/users/views/auth_view.py
+++ apps/users/views/auth_view.py
@@ -4,7 +4,6 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from apps.users.authentications import CookieJWTAuthentication
 from apps.users.serializers.signup_serializers import UserSignupSerializer
 from apps.users.services.exceptions import (
     EmailVerificationCodeFailedError,
