8df14210ab7d591f452dc680c257a1b8754e1868
inu9431 <inu9431@naver.com>
2025-09-22 15:08:11 +0900

    ✨ feat: 로그인 로그아웃 토큰 재발급 로직 생성 1.서비스에 재사용가능한 토큰발급,재발급, 로그아웃 코드 2. 뷰는 서비스 로직에 맟춰서 기능 작성 및 리프레시 토큰은 쿠키로 액세스토큰은 응답바디로 나가게 3.api에 맞춰서 테스트코드 작성 성공/실패 케이스

---
+++ apps/users/services/auth_service.py
@@ -0,0 +1,53 @@
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken, TokenError
+from rest_framework_simplejwt.settings import api_settings
+from rest_framework.exceptions import AuthenticationFailed, ValidationError
+
+from apps.users.models import User
+from typing import Dict
+from django.contrib.auth import authenticate
+from rest_framework.exceptions import AuthenticationFailed
+
+
+
+
+class AuthService:
+    """
+    이메일 로그인
+    """
+
+    @staticmethod
+    def email_login(email: str, password: str) -> dict[str, str]:
+        user =authenticate(email=email, password=password)
+
+        if not user:
+            raise AuthenticationFailed("이메일 또는 비밀번호가 틀립니다")
+        return RefreshToken.for_user(user)
+
+
+    @staticmethod
+    def revoke_refresh_tokens(refresh_token: str) -> None:
+        try:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+        except TokenError:
+            raise ValidationError("리프레시 토큰이 유효하지 않습니다")
+
+
+class JWTService:
+    @staticmethod
+    def refresh_access_token(refresh_token: str) -> str:
+        """
+        쿠키의 refresh 토큰으로 새로운 access 토큰 발급
+        """
+        if not refresh_token:
+            raise AuthenticationFailed("재로그인이 필요합니다")
+        try:
+            token = RefreshToken(refresh_token)
+            return str(token.access_token)
+        except Exception as e:
+            msg = str(e).lower()
+            if 'expired' in msg:
+                raise AuthenticationFailed("리프레시 토큰이 만료되었습니다")
+            raise AuthenticationFailed("유효하지 않은 토큰입니다")
+
--- apps/users/urls/auth_urls.py
+++ apps/users/urls/auth_urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
-from apps.users.views.auth_view import UserSignupAPIView
 from apps.users.views.email_verification_view import (
     AccountRecoveryEmailVerificationSendAPIView,
     AccountRecoveryEmailVerificationVerifyAPIView,
@@ -9,11 +9,12 @@ from apps.users.views.email_verification_view import (
     SignUpEmailVerificationSendAPIView,
     SignUpEmailVerifiCationVerifyAPIView,
 )
-from apps.users.views.phone_verification_view import (
-    SendVerificationCodeAPIView,
-    VerifyCodeAPIView,
-)
-from apps.users.views.withdrawals_view import AccountRecoveryAPIView, WithdrawalAPIView
+from apps.users.views.phone_verification_view import SendVerificationCodeAPIView, VerifyCodeAPIView
+from apps.users.views.auth_view import UserSignupAPIView, EmailLoginAPIView, CookieTokenRefreshAPIView
+from apps.users.views.auth_view import  LogoutAPIView
+from apps.users.views.withdrawals_view import WithdrawalAPIView
+
+from apps.users.views.withdrawals_view import AccountRecoveryAPIView
 
 urlpatterns = [
     path("auth/email/send-code", SignUpEmailVerificationSendAPIView.as_view(), name="email_send_code"),
@@ -32,9 +33,13 @@ urlpatterns = [
         AccountRecoveryEmailVerificationVerifyAPIView.as_view(),
         name="recover_account_verify",
     ),
+    path("auth/recover", AccountRecoveryAPIView.as_view(), name="account_recovery"),
+
     path("auth/phone/send-code", SendVerificationCodeAPIView.as_view(), name="phone_send_code"),
     path("auth/phone/verify", VerifyCodeAPIView.as_view(), name="phone_verify_code"),
     path("auth/withdraw", WithdrawalAPIView.as_view(), name="account_withdrawals"),
     path("auth/email/signup", UserSignupAPIView.as_view(), name="signup"),
-    path("auth/recover", AccountRecoveryAPIView.as_view(), name="account_recovery"),
+    path("auth/refresh", CookieTokenRefreshAPIView.as_view(), name="token_refresh"),
+    path("auth/email/login", EmailLoginAPIView.as_view(), name="email_login"),
+    path("auth/logout", LogoutAPIView.as_view(), name="logout"),
 ]
