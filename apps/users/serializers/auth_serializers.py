6e19942ca1e56a2e7381b8feebc101e638802004
inu9431 <inu9431@naver.com>
2025-09-23 16:14:56 +0900

    💡 chore  : 액세스토큰 응답시리얼라이저 지우고 인라인시리얼라이저로 반환하게 수정

--- apps/users/serializers/auth_serializers.py
+++ apps/users/serializers/auth_serializers.py
@@ -6,7 +6,3 @@ from rest_framework import serializers
 class EmailLoginSerializer(serializers.Serializer[dict[str, Any]]):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
-
-
-class AccessTokenSerializer(serializers.Serializer[dict[str, Any]]):
-    access_token = serializers.CharField()
