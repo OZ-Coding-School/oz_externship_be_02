ff2d90456fe1710df1cd7fb7b60acc46a9dcb891
inu9431 <inu9431@naver.com>
2025-09-23 14:19:41 +0900

    🎨 styl성e    : 코드 포매팅 수정

--- apps/users/serializers/auth_serializers.py
+++ apps/users/serializers/auth_serializers.py
@@ -7,5 +7,6 @@ class EmailLoginSerializer(serializers.Serializer[dict[str, Any]]):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
 
+
 class AccessTokenSerializer(serializers.Serializer[dict[str, Any]]):
     access_token = serializers.CharField()
