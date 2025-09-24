3f63f43604192632679c3df22a325d065befd0b9
Meoyoug <eodyd426@gmail.com>
2025-09-24 19:21:45 +0900

    ♻️ refactor : admin_url을 config.urls에서 include 하도록 수정

--- apps/lectures/urls/__init__.py
+++ apps/lectures/urls/__init__.py
@@ -5,5 +5,4 @@ from apps.lectures.urls.bookmarks_urls import urlpatterns as bookmark_urls
 
 urlpatterns: list[URLPattern | URLResolver] = [
     *bookmark_urls,
-    path("/admin/lectures", include((admin_urls, "lectures-admin"), namespace="lectures-admin")),
 ]
