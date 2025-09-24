9eda9f3a49e0420c5f7ba8da54f0be9db0abb1d4
inu9431 <inu9431@naver.com>
2025-09-24 16:31:04 +0900

    💡 chore  : mypy 해결

--- apps/users/models/__init__.py
+++ apps/users/models/__init__.py
@@ -2,4 +2,5 @@ from .social_user import SocialUser
 from .user import User
 from .withdrawals import Withdrawals, WithdrawalsReasonChoices
 
-__all__ = ["User", "SocialUser", "Withdrawals"]
+__all__ = ["User", "SocialUser", "Withdrawals", "WithdrawalsReasonChoices"
+]
