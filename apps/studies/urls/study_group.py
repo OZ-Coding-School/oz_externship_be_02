0b50346d18a5eee32c93f84399d58728219058b7
KIM SHINEDAY <kimshineday99@gmail.com>
2025-09-25 15:36:22 +0900

    ✨ feat: 스터디 그룹 리더 위임 View 작성 및 라우터 추가
    - view, 스터디 그룹 리더 위임 로직 작성
    - API 라우터 추가

--- apps/studies/urls/study_group.py
+++ apps/studies/urls/study_group.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from apps.studies.views.leader_delegate_view import StudyGroupLeaderDelegateView
 from apps.studies.views.study_group_create_view import CreateStudyGroupView
 from apps.studies.views.study_group_update_view import UpdateStudyGroupView
 
 urlpatterns = [
     path("", CreateStudyGroupView.as_view(), name="create_study_group"),
     path("/<uuid:group_uuid>", UpdateStudyGroupView.as_view(), name="update_study_group"),
+    path("/<uuid:group_uuid>/delegate", StudyGroupLeaderDelegateView.as_view(), name="leader_delegate"),
 ]
