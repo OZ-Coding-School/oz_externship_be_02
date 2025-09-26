a5c80cb15f689bf4c677f82a4e07f9ac256103a9
KIM SHINEDAY <kimshineday99@gmail.com>
2025-09-26 15:35:57 +0900

    ✨ feat: 스터디 그룹 탈퇴 view 작성 및 라우터 추가
    - 스터디 그룹 탈퇴 기능 구현을 위한 view 와 router 추가
    - delete 로직은 서비스 로직에 구현

--- apps/studies/urls/study_group.py
+++ apps/studies/urls/study_group.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from apps.studies.views.group_member_delete_view import WithdrawGroupMemberView
 from apps.studies.views.leader_delegate_view import StudyGroupLeaderDelegateView
 from apps.studies.views.study_group_create_view import CreateStudyGroupView
 from apps.studies.views.study_group_update_view import UpdateStudyGroupView
@@ -8,4 +9,5 @@ urlpatterns = [
     path("", CreateStudyGroupView.as_view(), name="create_study_group"),
     path("/<uuid:group_uuid>", UpdateStudyGroupView.as_view(), name="update_study_group"),
     path("/<uuid:group_uuid>/delegate", StudyGroupLeaderDelegateView.as_view(), name="leader_delegate"),
+    path("/<uuid:group_uuid>/withdraw", WithdrawGroupMemberView.as_view(), name="withdraw_study_group"),
 ]
