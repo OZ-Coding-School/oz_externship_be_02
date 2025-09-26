be86636e8accb6001770af578b071bf42c463dc8
KIM SHINEDAY <kimshineday99@gmail.com>
2025-09-26 15:39:26 +0900

    ✨ feat: 스터디 그룹 탈퇴 기능 구현
    - delete_member_service
      - 탈퇴 및 강퇴(추후 구현)시 해당 유저가 그룹 리더인지 확인하는 로직 <check_leader>
      - 그룹 리더가 탈퇴 요청 시 에러 발생.

---
+++ apps/studies/services/delete_member_service.py
@@ -0,0 +1,29 @@
+from rest_framework.exceptions import PermissionDenied
+
+from apps.studies.models.study_groups import StudyGroup
+from apps.users.models import User
+
+
+class DeleteMemberService:
+    """
+    스터디 그룹 멤버 삭제 서비스
+    그룹 탈퇴, 강퇴 기능 구현.
+    """
+
+    def __init__(self, group: StudyGroup, user: User) -> None:
+        self.group = group
+        self.target = user
+
+    def check_leader(self) -> bool:
+        """
+        탈퇴, 강퇴 요청 시 해당되는 유저가 그룹 리더인지 체크
+        """
+        return self.group.members.through.objects.get(user=self.target, study_group=self.group).is_leader
+
+    def studygroup_withdraw_service(self) -> None:
+        """
+        스터디 그룹 탈퇴 기능
+        """
+        if self.check_leader():
+            raise PermissionDenied("그룹 리더는 리더 위임 후 그룹을 탈퇴할 수 있습니다.")
+        self.group.members.remove(self.target)
