508283e34ebb19a269fc31cc4680e6dc26d2c0d0
Meoyoug <eodyd426@gmail.com>
2025-09-26 19:18:56 +0900

    style: 코드 포매터 적용 및 코드 수정

--- apps/users/services/kakao_login_service.py
+++ apps/users/services/kakao_login_service.py
@@ -98,15 +98,13 @@ class KakaoService:  # 카카오 로그인 로직 담당 클래스
 
         validated_data: dict[str, Any] = serializer.validated_data.copy()
         validated_data["birthday"] = cls.parse_kakao_birth_date(
-            validated_data["kakao_account"].pop("birthday"), validated_data["kakao_account"].pop("birthyear")
+            validated_data["kakao_account"].pop("birthday"),
+            validated_data["kakao_account"].pop("birthyear", str(timezone.now().year)),
         )
         return validated_data
 
     @classmethod
-    def parse_kakao_birth_date(cls, birth_day: Optional[str], birth_year: Optional[str]) -> date:
-        if not birth_year:
-            birth_year = timezone.now().year
-
+    def parse_kakao_birth_date(cls, birth_day: str, birth_year: str) -> date:
         day = birth_day[:2]
         month = birth_day[2:]
         birthday_string = f"{birth_year}-{month}-{day}"
