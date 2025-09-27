b4eff3e3f0b36f49bee6e215237b05d637e31e4d
inu9431 <inu9431@naver.com>
2025-09-27 19:29:33 +0900

    🎨 style    : 코드 포매팅 수정

--- apps/users/tasks.py
+++ apps/users/tasks.py
@@ -1,17 +1,18 @@
 import logging
-from celery import shared_task
 from datetime import date
-from django.db import transaction
+
+from celery import shared_task
 from django.contrib.auth import get_user_model
+from django.db import transaction
 
 from apps.users.models.withdrawals import Withdrawals
-from apps.users.models.user import User
 
 User = get_user_model()
-logger = logging.getLogger(__name__) # 테스크의 파일 로거 생성
+logger = logging.getLogger(__name__)  # 테스크의 파일 로거 생성
+
 
 @shared_task
-def delete_expired_withdrawals_users():
+def delete_expired_withdrawals_users() -> None:
     """
     탈퇴 유예 기간(due_date)이 지난 사용자 계정을 영구 삭제
     """
@@ -30,11 +31,12 @@ def delete_expired_withdrawals_users():
                     if user_to_delete:
                         user_email = user_to_delete.email
                         user_to_delete.delete()
-                        logger.info(f"사용자 계정 삭제: {user_email}") # info 레벨로 성공 로그
+                        logger.info(f"사용자 계정 삭제: {user_email}")  # info 레벨로 성공 로그
                 except User.DoesNotExist:
                     logger.warning(f"이미 삭제되었거나 존재하지 않는 사용자입니다. (탈퇴 요청 ID: {withdrawal.id})")
                 except Exception as e:
-                    logger.error(f"사용자 계정 삭제 중 오류 발생 (탈퇴 요청 ID: {withdrawal.id}, 오류: {e})", exc_info=True)
+                    logger.error(
+                        f"사용자 계정 삭제 중 오류 발생 (탈퇴 요청 ID: {withdrawal.id}, 오류: {e})", exc_info=True
+                    )
     else:
-        logger.info("만료된 탈퇴 요청이 없습니다") # 정상적인 상황 기록
-
+        logger.info("만료된 탈퇴 요청이 없습니다")  # 정상적인 상황 기록
