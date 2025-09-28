3d4a7836db0a37e84acb152370d9b684d2125633
Umdoong <umdoongs@gmail.com>
2025-09-28 18:00:24 +0900

    ✨ feat: get에서는 None을 반환하는 JWTAuthentication 생성 후 적용

---
+++ apps/core/authentications.py
@@ -0,0 +1,19 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import exceptions
+
+
+class JWTAuthenticationOrReadOnly(JWTAuthentication):
+    """
+    GET 메서드: 인증 미적용 (공개 접근 허용)
+    다른 메서드: JWT 인증 적용 (토큰 미제공 시 에러 발생)
+    """
+
+    def authenticate(self, request):
+        if request.method == 'GET':
+            return None  # GET은 인증 스킵
+
+        auth_result = super().authenticate(request)
+        if auth_result is None:
+            # 토큰이 없으면 명시적 에러 발생
+            raise exceptions.AuthenticationFailed('Authentication credentials were not provided.')
+        return auth_result
