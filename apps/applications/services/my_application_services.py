18849368fa92d84a959ce0236b71a71a7ef2b2ea
daeun0608 <glace0608@naver.com>
2025-09-28 17:27:39 +0900

    ✨  feat     : 에러 처리 추가 get를 활용하는 DoesNotExist 예외 처리 관련 이슈: #241

--- apps/applications/services/my_application_services.py
+++ apps/applications/services/my_application_services.py
@@ -3,15 +3,18 @@ from apps.users.models.user import User
 
 
 def get_my_detail_aply(user: User, application_id: int) -> Application:
-    aply = Application.objects.get(user=user, id=application_id)
+    try:
+        aply = Application.objects.get(user=user, id=application_id)
+    except Application.DoesNotExist:
+        aply = None
     return aply
 
 
 def cancel_my_aply(user: User, application_id: int) -> bool:
     aply = get_my_detail_aply(user, application_id)
-    if aply.status == Application.ApplicationStatus.PENDING:
+    if aply is None or aply.status != Application.ApplicationStatus.PENDING:
+        return False
+    else:
         aply.status = Application.ApplicationStatus.CANCELED
         aply.save()
         return True
-    else:
-        return False
