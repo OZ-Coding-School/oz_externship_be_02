e343e70453867e31395b9ee0d2634c02921d4506
Dayeon <0812dayeon@gmail.com>
2025-09-29 22:57:13 +0900

    🐛 fix: url 오류 수정
    urls.py에서 생긴 오류를 수정하였음.

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import ChatMessageListView, ChatRoomListView
 
 urlpatterns = [
-    path("/rooms/", ChatRoomListView.as_view(), name="chatroom-list"),
-    path("/rooms/<str:study_group_uuid>/messages/", ChatMessageListView.as_view(), name="chat-message-by-room"),
+    path("/rooms", ChatRoomListView.as_view(), name="chatroom-list"),
+    path("/<uuid:study_group_uuid>/messages", ChatMessageListView.as_view(), name="message-list"),
 ]
