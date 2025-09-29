5bc8c27924ef33740c61d8fd2f0d2ec37f91c77c
Dayeon <0812dayeon@gmail.com>
2025-09-29 22:31:27 +0900

    🐛 fix      : 버그 수정
    api 문서에 맞도록 응답 수정 및 isort, black을 했음.

--- apps/chat/serializers.py
+++ apps/chat/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from rest_framework.pagination import CursorPagination
+
 from ..studies.models import StudyGroup
 from ..users.models import User
 from .models import ChatMessage
@@ -8,7 +8,7 @@ from .models import ChatMessage
 class SenderInfoSerializer(serializers.ModelSerializer[User]):
     class Meta:
         model = User
-        fields = ["uuid", "nickname", "name", "gender"]
+        fields = ["uuid", "nickname", "name", "gender", "profile_img_url"]
 
 
 class ChatMessageSerializer(serializers.ModelSerializer[ChatMessage]):
@@ -18,13 +18,6 @@ class ChatMessageSerializer(serializers.ModelSerializer[ChatMessage]):
         model = ChatMessage
         fields = ["id", "content", "sender", "created_at"]
 
-class ChatMessageCursorPagination(CursorPagination):
-    # 채팅 메시지용 커서 페이지 네이션
-    page_size = 100
-    ordering = 'created_at'
-    cursor_query_param = 'cursor'
-    page_size_query_param = 'page_size'
-    max_page_size = 300
 
 class ChatRoomSerializer(serializers.ModelSerializer[StudyGroup]):
     last_message = ChatMessageSerializer(
@@ -35,27 +28,3 @@ class ChatRoomSerializer(serializers.ModelSerializer[StudyGroup]):
     class Meta:
         model = StudyGroup
         fields = ["uuid", "name", "unread_message_count", "last_message"]
-
-
-class SenderSerializer(serializers.ModelSerializer[User]):
-    user_uuid = serializers.UUIDField(source="uuid", read_only=True)
-    profile_img_url = serializers.SerializerMethodField()
-
-    class Meta:
-        model = User
-        fields = ["uuid", "nickname", "profile_img_url"]
-        extra_kwargs = {"uuid": {"read_only": True}}
-
-    def get_profile_img_url(self, obj: User) -> str:
-        if hasattr(obj, "profile_image") and obj.profile_image:
-            return f"~/profiles/{obj.uuid}.png"
-        return f"~/profiles/default.png"  # 기본 이미지
-
-
-class ApiChatMessageSerializer(serializers.ModelSerializer[ChatMessage]):
-    message_id = serializers.IntegerField(source="id", read_only=True)
-    sender = SenderSerializer(read_only=True)
-
-    class Meta:
-        model = ChatMessage
-        fields = ["message_id", "sender", "content", "created_at"]
