b7075dacf18c0a491f0825f0df17cb33f2dc98a4
jclee5419 <jclee5419@gmail.com>
2025-09-30 00:46:49 +0900

    ✨ feat     : 그룹 스케줄 시리얼라이저 추가

--- apps/study_group_schedules/serializers/__init__.py
+++ apps/study_group_schedules/serializers/__init__.py
@@ -1,9 +1,11 @@
 from .schedule_serializers import (
     StudyGroupScheduleCreateSerializer,
     StudyGroupScheduleResponseSerializer,
+    StudyGroupScheduleUpdateSerializer,
 )
 
 __all__ = [
     "StudyGroupScheduleCreateSerializer",
     "StudyGroupScheduleResponseSerializer",
+    "StudyGroupScheduleUpdateSerializer",
 ]
