7fb0d54a0ac210d58ec0cec8e9b794f3126d0e2e
c0z-y-fiction <cosm0snnt@gmail.com>
2025-09-30 01:59:11 +0900

    ✨ feat: 이메일 찾기 URL 추가
    - find_email_send_code
    - find_email_verify_code

--- apps/users/urls/auth_urls.py
+++ apps/users/urls/auth_urls.py
@@ -17,6 +17,8 @@ from apps.users.views.email_verification_view import (
 from apps.users.views.phone_verification_view import (
     SendVerificationCodeAPIView,
     VerifyCodeAPIView,
+    FindEmailSendCodeAPIView,
+    FindEmailVerifyCodeAPIView,
 )
 from apps.users.views.social_login_views import (
     KakaoLoginCallbackView,
@@ -46,6 +48,8 @@ urlpatterns = [
     path("auth/recover", AccountRecoveryAPIView.as_view(), name="account_recovery"),
     path("auth/phone/send-code", SendVerificationCodeAPIView.as_view(), name="phone_send_code"),
     path("auth/phone/verify", VerifyCodeAPIView.as_view(), name="phone_verify_code"),
+    path("auth/find-email/send-code", FindEmailSendCodeAPIView.as_view(), name="find_email_send_code"),
+    path("auth/find-email/verify", FindEmailVerifyCodeAPIView.as_view(), name="find_email_verify_code"),
     # 회원 탈퇴 관련
     path("auth/withdraw", WithdrawalAPIView.as_view(), name="account_withdrawals"),
     # 회원 가입 및 로그인 관련
