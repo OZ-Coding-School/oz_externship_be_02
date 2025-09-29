ac38cd2e6acb35001154300e4885ffa3a56eb4ac
00475-AEDF-52510-2 <d050586458@gmail.com>
2025-09-29 16:57:21 +0900

    ✨ feat: config/asgi.py파일 설정
    ProtocolTypeRouter 설정 및 chat 앱 라우팅 설정
    
    관련 이슈: #256

--- config/asgi.py
+++ config/asgi.py
@@ -9,8 +9,22 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+import apps.chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        # WebSocket 요청은 Channels의 인증 미들웨어와 URL 라우터를 통해 처리
+        "websocket": AuthMiddlewareStack(
+            URLRouter(apps.chat.routing.websocket_urlpatterns)
+        ),
+    }
+)
