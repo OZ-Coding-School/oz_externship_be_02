de28fe741eb69a7325f2b73911046fd88487d5a2
c0z-y-fiction <cosm0snnt@gmail.com>
2025-09-30 15:25:12 +0900

    ✨ feat: 이메일 찾기 API URL 패턴 추가
    - /info/find-email/ 경로에 FindEmailView 연결.

--- apps/users/urls/users_urls.py
+++ apps/users/urls/users_urls.py
@@ -1,8 +1,10 @@
 from django.urls import URLPattern, URLResolver, path
 
+from apps.users.views.find_email_view import FindEmailView
 from apps.users.views.user_info_view import UserInfoEditView, UserInfoView
 
 urlpatterns: list[URLPattern | URLResolver] = [
     path("info/", UserInfoView.as_view(), name="user_info"),
     path("info/edit/", UserInfoEditView.as_view(), name="user_info_edit"),
+    path("info/find-email/", FindEmailView.as_view(), name="find_email"),
 ]
