cf0f301c77847a749ff537b4be2ac4e2633d1024
00475-AEDF-52510-2 <d050586458@gmail.com>
2025-09-30 17:47:00 +0900

    🐛 fix: 일관성 위한 routing 수정
    다연님의 api와 통일시키기 위해 routing을 수정하여 일관성을 챙겼습니다.
    
    관련 이슈: #256

--- apps/chat/routing.py
+++ apps/chat/routing.py
@@ -6,9 +6,8 @@ from django.urls import URLPattern, path
 from . import consumers
 
 websocket_urlpatterns: List[URLPattern] = [
-    # ws/chat/<스터디그룹UUID>/ 경로로 들어오는 WebSocket 요청을 ChatConsumer가 처리하도록 설정
     path(
-        "ws/chat/<uuid:study_group_uuid>/",
+        "ws/chat/rooms/<uuid:study_group_uuid>/",
         consumers.ChatConsumer.as_asgi(),  # type: ignore
         name="ws_chat_room",
     ),
