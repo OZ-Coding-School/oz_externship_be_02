71a992b3a3cdd7d141c4bdd734694309484d5c44
inu9431 <inu9431@naver.com>
2025-09-30 14:15:17 +0900

    ✨ feat   : 유저 데이터 반환하기위해 응답 시리얼라이저 작성

--- apps/users/serializers/auth_serializers.py
+++ apps/users/serializers/auth_serializers.py
@@ -2,7 +2,32 @@ from typing import Any
 
 from rest_framework import serializers
 
+from apps.users.models import User
 
-class EmailLoginSerializer(serializers.Serializer[dict[str, Any]]):
+
+class EmailLoginRequestSerializer(serializers.Serializer[dict[str, Any]]):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
+
+class LoginResponseSerializer(serializers.ModelSerializer[User]):
+    class Meta:
+        model = User
+        fields = [
+            "email",
+            "password",
+            "nickname",
+            "name",
+            "birthday",
+            "gender",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+            "created_at",
+            "updated_at",
+        ]
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+
+
+
